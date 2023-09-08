7688e8b775ea8a58184ff3e0caaf6521e39b11e2
sparkwud <kelvinagbenyo@gmail.com>
2023-09-08 23:05:45 +0000

    homescreen coming togetherrr

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:golly_express/cards/track_package_card.dart';
+import 'package:golly_express/cards/verify_id_card.dart';
 
 import '../components/input_field.dart';
 
@@ -9,14 +10,33 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Container(
-          width: double.infinity,
-          color: const Color(0xFfE6EAED),
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(6, (index) => Text("$index")),
-          )),
+    return const Scaffold(
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 150,
+            horizontal: 16.0,
+          ),
+          child: Column(
+            children: [
+              VerifyIDCard(),
+              SizedBox(height: 16),
+              TrackPackageCard(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+// old stuff
+// Container(
+//         width: double.infinity,
+//         color: const Color(0xFfE6EAED),
+//         child: GridView.count(
+//           crossAxisCount: 3,
+//           children: List.generate(6, (index) => Text("$index")),
+//         ),
+//       ),
