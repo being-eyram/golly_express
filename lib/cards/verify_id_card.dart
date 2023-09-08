05aa34cb774685b8f9129814e66c99c4df675ca3
sparkwud <kelvinagbenyo@gmail.com>
2023-09-08 18:51:57 +0000

    create verify ID card

---
+++ lib/cards/verify_id_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class VerifyIDCard extends StatelessWidget {
+  const VerifyIDCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userID = Image.asset(
+      "assets/images/user_id.png",
+      width: 45,
+      height: 45,
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: const Color(0xFFFA0101),
+          width: 1,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 12.0,
+          horizontal: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            userID,
+            const SizedBox(width: 16),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Verify your ID",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Text(
+                  "A valid Ghana card is required to verify\n your account",
+                  style: TextStyle(
+                    color: Color(0xFFA3ADAA),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(width: 50),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_forward_ios),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
