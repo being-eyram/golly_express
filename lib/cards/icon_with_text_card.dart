7fd6180d23126f6e3c2459f79d8e5eeb26582c8d
sparkwud <kelvinagbenyo@gmail.com>
2023-09-09 17:25:05 +0000

    create card for menu tabs

---
+++ lib/cards/icon_with_text_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class IconWithTextCard extends StatelessWidget {
+  const IconWithTextCard({
+    super.key,
+    required this.cardTitle,
+    required this.cardIcon,
+    required this.cardColor,
+    required this.iconContainerColor,
+  });
+  final String cardTitle;
+  final SvgPicture cardIcon;
+  final Color cardColor;
+  final Color iconContainerColor;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // ...
+      height: 86,
+      width: 167,
+      padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        // color: const Color(0xFFFFFBEE),
+        // color: Colors.green,
+        color: cardColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              // color: Color(0xFFFFC727),
+              color: iconContainerColor,
+            ),
+            child: cardIcon,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            cardTitle,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
