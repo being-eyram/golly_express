983105fda56045b64343f24ced366e80d34652ae
sparkwud <kelvinagbenyo@gmail.com>
2023-09-09 20:29:36 +0000

    update assets with bottom navigation bar icons

---
+++ lib/constants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+final gollyExpressLogo = Image.asset(
+  'assets/images/golly_express.jpeg',
+  width: 98,
+  height: 91,
+);
+final googleLogo = SvgPicture.asset(
+  'assets/images/ic_google.svg',
+  semanticsLabel: 'Google Logo',
+);
+final appleLogo = SvgPicture.asset(
+  'assets/images/ic_apple.svg',
+  semanticsLabel: 'Apple Logo',
+);
+
+final calculatorSVG = SvgPicture.asset(
+  'assets/images/ic_calculator.svg',
+  semanticsLabel: 'Calculator',
+);
+final cargoShipSVG = SvgPicture.asset(
+  'assets/images/ic_cargo_ship.svg',
+  semanticsLabel: 'Calculator',
+);
+
+final userIDIcon = Image.asset(
+  "assets/images/user_id.png",
+  width: 45,
+  height: 45,
+);
