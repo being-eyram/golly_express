a62140ee46f661232defdf3317a5c31cab06e6f2
sparkwud <kelvinagbenyo@gmail.com>
2023-09-12 18:15:29 +0000

    create packages screen

--- lib/screens/packages_screen.dart
+++ lib/screens/packages_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:golly_express/constants.dart';
 
 class PackageScreen extends StatelessWidget {
   const PackageScreen({super.key});
@@ -36,69 +37,116 @@ class PackageScreen extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 16.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Color(0xFFFAFBFB),
-                ),
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Product Name Goes Here",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                      ),
+
+              //
+              Stack(
+                // alignment: Alignment.topRight,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      // color: Color(0xFFFAFBFB),
+                      color: Colors.grey,
                     ),
-                    const SizedBox(height: 7),
-                    Text(
-                      "#1234455",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w700,
-                        color: Color(0xFFA3ADAA),
-                      ),
-                    ),
-                    SizedBox(height: 40.0),
-                    FilledButton(
-                      style: FilledButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 8,
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Product Name Goes Here",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
-                        foregroundColor: Color(0xFF398B79),
-                        backgroundColor: Color(0xFFE3F2EF),
-                      ),
-                      onPressed: () {},
-                      child: Row(
-                        children: [
-                          Text("Received in Office"),
-                          SizedBox(width: 10),
-                          Container(
-                            alignment: Alignment.center,
-                            // height: 16,
-                            // width: 16,
-                            padding: EdgeInsets.all(3),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.white,
+                        const SizedBox(height: 7),
+                        Text(
+                          "#1234455",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFFA3ADAA),
+                          ),
+                        ),
+                        SizedBox(height: 40.0),
+                        FilledButton(
+                          style: FilledButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
                             ),
-                            child: Icon(
-                              Icons.arrow_forward_ios,
-                              size: 10,
-                            ),
-                          )
-                        ],
-                      ),
+                            foregroundColor: Color(0xFF398B79),
+                            backgroundColor: Color(0xFFE3F2EF),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text("Received in Office"),
+                              SizedBox(width: 10),
+                              Container(
+                                alignment: Alignment.center,
+                                // height: 16,
+                                // width: 16,
+                                padding: EdgeInsets.all(3),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
+                  ),
+                  Positioned(
+                    bottom: -60,
+                    right: -60,
+                    child: packageVector,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              TrackingHistory()
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class TrackingHistory extends StatelessWidget {
+  const TrackingHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Tracking History",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        const SizedBox(height: 16),
+        PackageCard()
+      ],
+    );
+  }
+}
+
+class PackageCard extends StatelessWidget {
+  const PackageCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
   }
 }
