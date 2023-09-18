d591dcf9e9cf0d353cdf2686c90e9528c362914c
sparkwud <kelvinagbenyo@gmail.com>
2023-09-18 15:24:48 +0000

    rename method

---
+++ lib/components/package_tracking_history.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:golly_express/cards/package_card.dart';
+import 'package:golly_express/constants.dart';
+
+class PackageTrackingHistory extends StatelessWidget {
+  const PackageTrackingHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Tracking History",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Eyram's MacBook Pro",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Joe's Petite",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+        const SizedBox(height: 16),
+        PackageCard(
+          packageName: "Product Name Goes Here",
+          packageID: "#1234455",
+          packageIcon: genericPackageIcon,
+        ),
+      ],
+    );
+  }
+}
