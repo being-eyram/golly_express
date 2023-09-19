54a5f389154afcac1b4c775e8e1a279f519d15e0
sparkwud <kelvinagbenyo@gmail.com>
2023-09-19 18:54:59 +0000

    profile screen ....

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:golly_express/constants.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xFFE6EDB7),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "My Profile",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 37),
+                Row(
+                  children: [
+                    userProfile,
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Ben-Aaron Mills-Pappoe",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                        SizedBox(height: 6),
+                        Text(
+                          "+233 57 159 2866",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    FilledButton(
+                      style: FilledButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 10)),
+                      onPressed: () {},
+                      child: Text(
+                        "Edit",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
