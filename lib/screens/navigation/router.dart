c750b606025d791311c683a97df001a30c94c240
sparkwud <kelvinagbenyo@gmail.com>
2023-09-20 19:50:15 +0000

    ....profile screen

--- lib/screens/navigation/router.dart
+++ lib/screens/navigation/router.dart
@@ -9,21 +9,23 @@ import 'package:golly_express/shared_prefs/shared_prefs.dart';
 import '../home.dart';
 import '../signup.dart';
 
-final GoRouter appRouter = GoRouter(routes: <RouteBase>[
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const ProfileScreen(),
-    // redirect: (context, state) async {
-    //   var didOnboard = await getUserOnboardStatus() ?? false;
-    //   return didOnboard ? '/signin' : null;
-    // },
-  ),
-  GoRoute(
-    path: '/signin',
-    builder: (context, state) => const SigninScreen(),
-  ),
-  GoRoute(
-    path: '/signup',
-    builder: (context, state) => const SignUpScreen(),
-  ),
-]);
+final GoRouter appRouter = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ProfileScreen(),
+      // redirect: (context, state) async {
+      //   var didOnboard = await getUserOnboardStatus() ?? false;
+      //   return didOnboard ? '/signin' : null;
+      // },
+    ),
+    GoRoute(
+      path: '/signin',
+      builder: (context, state) => const SigninScreen(),
+    ),
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => const SignUpScreen(),
+    ),
+  ],
+);
