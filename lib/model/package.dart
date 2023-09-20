b93fa4f5b96c40e77552da36158d40c3dcfabd53
sparkwud <kelvinagbenyo@gmail.com>
2023-09-20 15:15:33 +0000

    package class refactor

---
+++ lib/model/package.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:golly_express/constants.dart';
+
+class Package {
+  Package({
+    this.name,
+    this.id,
+    this.icon,
+  });
+
+  String? name;
+  String? id;
+  SvgPicture? icon;
+}
+
+// recent/ tracking packages
+var recentPackages = [
+  Package(
+    name: "Eyram's MacBook Pro",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Joe's Petite",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+  Package(
+    name: "Product Name Goes Here",
+    id: "#1234455",
+    icon: genericPackageIcon,
+  ),
+];
