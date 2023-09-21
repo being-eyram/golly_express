217aea8a4c307b8dbf78d4fce9cd4194412fecca
sparkwud <kelvinagbenyo@gmail.com>
2023-09-21 20:42:54 +0000

    style tab menues

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -183,6 +183,41 @@ class ProfileScreen extends StatelessWidget {
                     ],
                   ),
                 ),
+
+                // new side
+                SizedBox(width: 16),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Color(0xFFEDEFEE),
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "32",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        "Received",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           )
