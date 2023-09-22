ded26d03d2e0fb2fdce44647cb18a9d3cb2e8334
sparkwud <kelvinagbenyo@gmail.com>
2023-09-22 18:16:53 +0000

    add custom app icons

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,6 +7,12 @@ final gollyExpressLogo = Image.asset(
   height: 91,
 );
 
+final gollyExpressLogoMini = Image.asset(
+  'assets/images/golly_express.jpeg',
+  width: 50,
+  height: 50,
+);
+
 final userProfile = SvgPicture.asset(
   'assets/images/ic_user_profile.svg',
   semanticsLabel: 'User Profile',
