ca674b8ed17765ffe055da02ae335e2c65d28272
sparkwud <kelvinagbenyo@gmail.com>
2023-09-22 20:13:55 +0000

    extract transaction summary

---
+++ lib/model/transaction_summary.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class TransactionSummary extends StatelessWidget {
+  const TransactionSummary({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              // width: 103.67,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1.5,
+                  color: const Color(0xFFEDEFEE),
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Column(
+                // mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "32",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "To Receieve",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      // fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Container(
+              // width: 103.67,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1.5,
+                  color: const Color(0xFFEDEFEE),
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Column(
+                // mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "32",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Total Orders",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      // fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Container(
+              // width: 103.67,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1.5,
+                  color: const Color(0xFFEDEFEE),
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Column(
+                children: [
+                  Text(
+                    "32",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Received",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      // fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
