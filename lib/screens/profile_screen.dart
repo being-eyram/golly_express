2432df309db9cb42c860ae70ac018a87bd8ed47b
sparkwud <kelvinagbenyo@gmail.com>
2023-09-22 18:17:17 +0000

    add id verification list tile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -79,6 +79,64 @@ class ProfileScreen extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           TransactionSummary(),
+          const SizedBox(height: 24),
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 32),
+            leading: gollyExpressLogoMini,
+            title: Text(
+              "Your Golly Express Address",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 32),
+            leading: Icon(Icons.location_on_outlined),
+            title: Text(
+              "My Address",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.symmetric(
+                horizontal: BorderSide(
+                  width: 2,
+                  color: Color(0xFFF6F7F6),
+                ),
+              ),
+            ),
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 32),
+              leading: Icon(Icons.badge_outlined),
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "ID Verification",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              subtitle: Text(
+                "A valid Ghana card is required to verify your account",
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFFF3AA60),
+                ),
+              ),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
         ],
       ),
     );
