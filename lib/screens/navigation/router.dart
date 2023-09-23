ae743d2b02d7e2f3067dbecf072fa79cbe3c9b10
sparkwud <kelvinagbenyo@gmail.com>
2023-09-23 17:59:33 +0000

    show golly express address screen

--- lib/screens/navigation/router.dart
+++ lib/screens/navigation/router.dart
@@ -1,19 +1,14 @@
 import 'package:go_router/go_router.dart';
-import 'package:golly_express/screens/current_shipment_screen.dart';
-import 'package:golly_express/screens/packages_screen.dart';
-import 'package:golly_express/screens/profile_screen.dart';
+import 'package:golly_express/screens/golly_express_address_screen.dart';
 import 'package:golly_express/screens/signin.dart';
-import 'package:golly_express/screens/onboarding.dart';
-import 'package:golly_express/shared_prefs/shared_prefs.dart';
 
-import '../home.dart';
 import '../signup.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
-      builder: (context, state) => const ProfileScreen(),
+      builder: (context, state) => const GollyEpxressAddressScreen(),
       // redirect: (context, state) async {
       //   var didOnboard = await getUserOnboardStatus() ?? false;
       //   return didOnboard ? '/signin' : null;
