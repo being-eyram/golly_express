e6b4de7a7639e2ed4cb3b8cd1721cbeac3734c0b
sparkwud <kelvinagbenyo@gmail.com>
2023-09-23 18:47:36 +0000

    add address line 1

--- lib/screens/golly_express_address_screen.dart
+++ lib/screens/golly_express_address_screen.dart
@@ -12,19 +12,57 @@ class GollyEpxressAddressScreen extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               "Current Shipment",
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  width: 1.5,
+                  color: const Color(0xFFEDEFEE),
+                ),
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Address Line 1",
+                    style: TextStyle(
+                      color: Color(0xFFA3ADAA),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Text(
+                        "2507 Investors Row, STE 100 Unit D5",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(Icons.copy),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
