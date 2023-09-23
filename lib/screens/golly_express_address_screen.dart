f3d23057599cbdd859d3fc44f4633e22cb00c34a
sparkwud <kelvinagbenyo@gmail.com>
2023-09-23 19:02:09 +0000

    use address line container model

--- lib/screens/golly_express_address_screen.dart
+++ lib/screens/golly_express_address_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:golly_express/model/address_line_container.dart';
 
 class GollyEpxressAddressScreen extends StatelessWidget {
   const GollyEpxressAddressScreen({super.key});
@@ -12,57 +13,28 @@ class GollyEpxressAddressScreen extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
+            Text(
               "Current Shipment",
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            const SizedBox(height: 16.0),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  width: 1.5,
-                  color: const Color(0xFFEDEFEE),
-                ),
-              ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Address Line 1",
-                    style: TextStyle(
-                      color: Color(0xFFA3ADAA),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Text(
-                        "2507 Investors Row, STE 100 Unit D5",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Spacer(),
-                      Icon(Icons.copy),
-                    ],
-                  ),
-                ],
-              ),
-            )
+            SizedBox(height: 16.0),
+            AddressLineContainer(
+              title: "Address Line 1",
+              subtitle: "2507 Investors Row, STE 100 Unit D5",
+            ),
+            SizedBox(height: 16),
+            AddressLineContainer(
+              title: "Address Line 2",
+              subtitle: "1904",
+            ),
           ],
         ),
       ),
