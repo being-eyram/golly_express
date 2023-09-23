b2edbc201e65ab28f136fff8a9f4b1897501936e
sparkwud <kelvinagbenyo@gmail.com>
2023-09-23 19:01:35 +0000

    create address line container model

---
+++ lib/model/address_line_container.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AddressLineContainer extends StatelessWidget {
+  const AddressLineContainer({
+    super.key,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          width: 1.5,
+          color: const Color(0xFFEDEFEE),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        // address lines
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              color: Color(0xFFA3ADAA),
+              fontSize: 13,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Text(
+                subtitle,
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const Spacer(),
+              IconButton(
+                // add copy to clipboard on pressed
+                onPressed: () {},
+                icon: const Icon(Icons.copy),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
