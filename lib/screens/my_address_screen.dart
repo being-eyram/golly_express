d98ae9bbd1ebcfe30a0e8228e1632e12e2a1a360
sparkwud <kelvinagbenyo@gmail.com>
2023-09-24 20:47:06 +0000

    add my address container

--- lib/screens/my_address_screen.dart
+++ lib/screens/my_address_screen.dart
@@ -19,16 +19,22 @@ class MyAddressScreen extends StatelessWidget {
             ),
             child: FilledButton(
               style: FilledButton.styleFrom(
+                backgroundColor: const Color(0xFFE8E9E8),
                 minimumSize: const Size(double.infinity, 54),
                 maximumSize: const Size(double.infinity, 54),
                 shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
                 ),
               ),
               onPressed: () {},
               child: const Text(
-                "Save Changes",
-                style: TextStyle(fontSize: 14),
+                "Add Address",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xFFA3ADAA),
+                ),
               ),
             ),
           ),
@@ -42,13 +48,66 @@ class MyAddressScreen extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new),
         ),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [],
+              children: [
+                const Text(
+                  "My Address",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(height: 24.0),
+
+                // address container
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      width: 1.5,
+                      color: const Color(0xFFEDEFEE),
+                    ),
+                  ),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+                    // address lines
+                    children: [
+                      Text(
+                        "Home",
+                        style: TextStyle(
+                          color: Color(0xFFA3ADAA),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "Kpa-Aps Technologies, 6th Adote Obour st, Accra , Ghana",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
