014e8509f4c97b6d6cbe1b8957e753beaad7fdb7
sparkwud <kelvinagbenyo@gmail.com>
2023-09-26 19:58:33 +0000

    create custom navigation bar widget

--- lib/components/custom_navigation_bar.dart
+++ lib/components/custom_navigation_bar.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
 import 'package:flutter/material.dart';
+import 'package:golly_express/constants.dart';
 
 class CustomNavigationBar extends StatefulWidget {
   const CustomNavigationBar({super.key});
@@ -10,6 +13,46 @@ class CustomNavigationBar extends StatefulWidget {
 class _CustomNavigationBarState extends State<CustomNavigationBar> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    int position = 0;
+
+    void _onTap(int index) {
+      setState(() {
+        position = index;
+      });
+    }
+
+    return BottomNavigationBar(
+      currentIndex: position,
+      onTap: _onTap,
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: [
+        BottomNavigationBarItem(
+          // icon: Icon(Icons.home),
+          icon: navHome,
+          activeIcon: navHomeActive,
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          // icon: Icon(Icons.inventory_2_outlined),
+          icon: navPackages,
+          activeIcon: navPackagesActive,
+          label: "packages",
+        ),
+        BottomNavigationBarItem(
+          // icon: Icon(Icons.person_outline),
+          icon: navProfile,
+          activeIcon: navProfileActive,
+          label: "profile",
+        ),
+        BottomNavigationBarItem(
+          // icon: Icon(Icons.notifications_none),
+          icon: navNotifications,
+          activeIcon: navNotificationsActive,
+          label: "notifications",
+        ),
+      ],
+    );
   }
 }
