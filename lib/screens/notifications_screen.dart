e944489b5c26f334e64e572fb78f44541261453d
sparkwud <kelvinagbenyo@gmail.com>
2023-09-28 14:00:08 +0000

    use notifications card

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../cards/notification_card.dart';
+
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
 
@@ -11,11 +13,21 @@ class NotificationsScreen extends StatelessWidget {
         centerTitle: true,
         title: const Padding(
           padding: EdgeInsets.only(top: 30.0),
-          child: Text("Notifications",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w800,
-              )),
+          child: Text(
+            "Notifications",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
+        child: Column(
+          children: [
+            NotificationCard(),
+          ],
         ),
       ),
     );
