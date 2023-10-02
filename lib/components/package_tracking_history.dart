d9ea71816892241224f83887cce7be24dc27dd08
sparkwud <kelvinagbenyo@gmail.com>
2023-10-02 17:40:22 +0000

    create empty tracking history list view

--- lib/components/package_tracking_history.dart
+++ lib/components/package_tracking_history.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:golly_express/cards/package_card.dart';
+import 'package:golly_express/constants.dart';
 import 'package:golly_express/model/package.dart';
 
 class PackageTrackingHistory extends StatelessWidget {
@@ -21,6 +22,41 @@ class PackageTrackingHistory extends StatelessWidget {
         ),
         const SizedBox(height: 16),
         ...recentPackages.map((package) => PackageCard(package: package))
+      ],
+    );
+  }
+}
+
+class EmptyTrackingHistory extends StatelessWidget {
+  const EmptyTrackingHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Tracking History",
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        const SizedBox(height: 24),
+        Center(
+          child: Column(
+            children: [
+              trackingHistoryPackageIcon,
+              const Text(
+                "There are no tracking history",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
