a90ba9dd3fd104aa82047509bcae2019471f376b
sparkwud <kelvinagbenyo@gmail.com>
2023-10-02 11:16:02 +0000

    create address class

--- lib/model/my_address_container.dart
+++ lib/model/my_address_container.dart
@@ -1,66 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:golly_express/model/address.dart';
 
 class MyAddressContainer extends StatelessWidget {
   const MyAddressContainer({
     super.key,
-    required this.icon,
-    required this.addressTitle,
-    required this.addressLine,
+    required this.address,
+    this.onTap,
   });
 
-  final Icon icon;
-  final String addressTitle;
-  final String addressLine;
+  final Address address;
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          width: 1.5,
-          color: const Color(0xFFEDEFEE),
+    return InkWell(
+      borderRadius: BorderRadius.circular(8),
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            width: 1.5,
+            color: const Color(0xFFEDEFEE),
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
 
-              // address lines
-              children: [
-                Row(
-                  children: [
-                    icon,
-                    const SizedBox(width: 8),
-                    Text(
-                      addressTitle,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
+                // address lines
+                children: [
+                  Row(
+                    children: [
+                      address.icon,
+                      const SizedBox(width: 8),
+                      Text(
+                        address.addressTitle!,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  addressLine,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 8),
+                  Text(
+                    address.addressLine!,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Icon(
-            Icons.arrow_forward_ios,
-            size: 18,
-          ),
-        ],
+            const Icon(
+              Icons.arrow_forward_ios,
+              size: 18,
+            ),
+          ],
+        ),
       ),
     );
   }
