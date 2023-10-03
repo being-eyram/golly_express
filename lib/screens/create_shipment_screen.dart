c6c535d21052a77d8d466816a4d08e10ba0df8a2
sparkwud <kelvinagbenyo@gmail.com>
2023-10-03 20:58:20 +0000

    create shipment screen tbc

--- lib/screens/create_shipment_screen.dart
+++ lib/screens/create_shipment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CreateShipmentScreen extends StatelessWidget {
   const CreateShipmentScreen({super.key});
@@ -7,15 +8,33 @@ class CreateShipmentScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Padding(
-          padding: EdgeInsets.only(top: 30.0),
-          child: Text(
-            "Create Shipment",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w800,
-            ),
+        leading: IconButton(
+          // onPressed: () {
+          //   Navigator.pop(context);
+          // },
+          onPressed: () => context.pop(),
+
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 20,
+          ),
+        ),
+      ),
+      body: const SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                "Create Shipment",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(height: 24.0),
+            ]),
           ),
         ),
       ),
