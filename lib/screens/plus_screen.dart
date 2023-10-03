424fc2d1b1696231c70dee15cabe1f5d2e399ee4
sparkwud <kelvinagbenyo@gmail.com>
2023-10-03 13:08:47 +0000

    create plus screen... might not need anyways

---
+++ lib/screens/plus_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlusScreen extends StatelessWidget {
+  const PlusScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
