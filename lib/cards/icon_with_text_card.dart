010b4c4c7a16e01687d612016832404842a66695
sparkwud <kelvinagbenyo@gmail.com>
2023-10-03 20:09:07 +0000

    add on tap function to icon with text card

--- lib/cards/icon_with_text_card.dart
+++ lib/cards/icon_with_text_card.dart
@@ -8,46 +8,52 @@ class IconWithTextCard extends StatelessWidget {
     required this.cardIcon,
     required this.cardColor,
     required this.iconContainerColor,
+    required this.onTap,
   });
   final String cardTitle;
   final SvgPicture cardIcon;
   final Color cardColor;
   final Color iconContainerColor;
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        // ...
-        // height: 86,
-        // width: 167,
-        padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          // color: const Color(0xFFFFFBEE),
-          // color: Colors.green,
-          color: cardColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                // color: Color(0xFFFFC727),
-                color: iconContainerColor,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: onTap,
+        child: Container(
+          // ...
+          // height: 86,
+          // width: 167,
+          padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 16.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            // color: const Color(0xFFFFFBEE),
+            // color: Colors.green,
+            color: cardColor,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  // color: Color(0xFFFFC727),
+                  color: iconContainerColor,
+                ),
+                child: cardIcon,
               ),
-              child: cardIcon,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              cardTitle,
-              style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-              ),
-            )
-          ],
+              const SizedBox(height: 8),
+              Text(
+                cardTitle,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
