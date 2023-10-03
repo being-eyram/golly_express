42a22ee43f2799f2c2c1d4d817419923e73ac7b7
sparkwud <kelvinagbenyo@gmail.com>
2023-10-03 14:50:47 +0000

    add plus botton to bottom nav bar

--- lib/components/main_container.dart
+++ lib/components/main_container.dart
@@ -5,6 +5,7 @@ import 'package:golly_express/constants.dart';
 import 'package:golly_express/screens/home.dart';
 import 'package:golly_express/screens/notifications_screen.dart';
 import 'package:golly_express/screens/packages_screen.dart';
+import 'package:golly_express/screens/plus_screen.dart';
 import 'package:golly_express/screens/profile_screen.dart';
 
 class MainContainer extends StatefulWidget {
@@ -26,6 +27,8 @@ class _MainContainerState extends State<MainContainer> {
   final _screens = [
     const HomeScreen(),
     const PackageScreen(),
+    const PlusScreen(),
+    // _customBottomSheet(context),
     const ProfileScreen(),
     const NotificationsScreen()
   ];
@@ -41,25 +44,56 @@ class _MainContainerState extends State<MainContainer> {
         showUnselectedLabels: false,
         items: [
           BottomNavigationBarItem(
-            // icon: Icon(Icons.home),
             icon: navHome,
             activeIcon: navHomeActive,
             label: "Home",
           ),
           BottomNavigationBarItem(
-            // icon: Icon(Icons.inventory_2_outlined),
             icon: navPackages,
             activeIcon: navPackagesActive,
             label: "packages",
           ),
           BottomNavigationBarItem(
-            // icon: Icon(Icons.person_outline),
+            icon: Container(
+              padding: const EdgeInsets.all(9),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x19557A46),
+                    blurRadius: 8,
+                    offset: Offset(0, 0),
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: const Icon(Icons.add_outlined),
+            ),
+            activeIcon: Container(
+              padding: const EdgeInsets.all(9),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x19557A46),
+                    blurRadius: 8,
+                    offset: Offset(0, 0),
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: const Icon(Icons.add),
+            ),
+            label: "plus",
+          ),
+          BottomNavigationBarItem(
             icon: navProfile,
             activeIcon: navProfileActive,
             label: "profile",
           ),
           BottomNavigationBarItem(
-            // icon: Icon(Icons.notifications_none),
             icon: navNotifications,
             activeIcon: navNotificationsActive,
             label: "notifications",
