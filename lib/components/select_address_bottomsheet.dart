2c1919a3931d529e37c22db2cedec2481f411454
sparkwud <kelvinagbenyo@gmail.com>
2023-10-04 20:13:08 +0000

    create select address bottom sheet

---
+++ lib/components/select_address_bottomsheet.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:golly_express/model/address.dart';
+import 'package:golly_express/model/my_address_container.dart';
+
+class SelectAddressBottomSheet extends StatelessWidget {
+  const SelectAddressBottomSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.white,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 16,
+      ),
+      // height: 350,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 5,
+              width: 69,
+              decoration: BoxDecoration(
+                color: const Color(0xFFB5BDBB),
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text(
+                "Close",
+                style: TextStyle(
+                  color: Color(0xFFA3ADAA),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Select product category',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 16),
+          ...userAddresses
+              .map((address) => MyAddressContainer(address: address))
+        ],
+      ),
+    );
+  }
+}
