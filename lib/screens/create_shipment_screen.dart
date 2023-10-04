2fe15fd795c15a81a57bade34bc6ece27f28f354
sparkwud <kelvinagbenyo@gmail.com>
2023-10-04 19:50:12 +0000

    replace select address input field with a container

--- lib/screens/create_shipment_screen.dart
+++ lib/screens/create_shipment_screen.dart
@@ -21,16 +21,22 @@ class CreateShipmentScreen extends StatelessWidget {
             ),
             child: FilledButton(
               style: FilledButton.styleFrom(
+                backgroundColor: const Color(0xFFE8E9E8),
                 minimumSize: const Size(double.infinity, 54),
                 maximumSize: const Size(double.infinity, 54),
                 shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
                 ),
               ),
               onPressed: () {},
               child: const Text(
-                "Save Changes",
-                style: TextStyle(fontSize: 14),
+                "Create Shipment",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Color(0xFFA3ADAA),
+                    fontWeight: FontWeight.w800),
               ),
             ),
           ),
@@ -67,27 +73,42 @@ class CreateShipmentScreen extends StatelessWidget {
                     hintText: "Tracking number",
                   ),
                   const SizedBox(height: 24.0),
-                  const InputTextField(
-                    suffixIcon: Icon(Icons.expand_more),
-                    hintText: "Select address",
+                  // InputTextField(
+                  //   suffixIcon: Icon(Icons.expand_more),
+                  //   hintText: "Select address",
+                  // ),
+                  InkWell(
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: const Color(0xFFEDEFEE),
+                        ),
+                      ),
+                      child: const Row(
+                        children: [
+                          Text(
+                            "Select address",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFFA3ADAA),
+                            ),
+                          ),
+                          Spacer(),
+                          Icon(Icons.expand_more)
+                        ],
+                      ),
+                    ),
                   ),
                   const SizedBox(height: 24.0),
                   const InputTextField(
                     hintText: "Describe product",
                   ),
                   const SizedBox(height: 24),
-                  Flexible(
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      // height: 50,
-                      // height: 200,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
