c54e7e3fdf007e82e5f86bae8d10b914256d3040
sparkwud <kelvinagbenyo@gmail.com>
2023-10-04 19:37:47 +0000

    expanding container in a column

--- lib/screens/create_shipment_screen.dart
+++ lib/screens/create_shipment_screen.dart
@@ -8,13 +8,37 @@ class CreateShipmentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(color: Color(0xFFEDEFEE)),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 16,
+              top: 16,
+              bottom: 24.0,
+            ),
+            child: FilledButton(
+              style: FilledButton.styleFrom(
+                minimumSize: const Size(double.infinity, 54),
+                maximumSize: const Size(double.infinity, 54),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Save Changes",
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+          ),
+        ],
+      ),
       appBar: AppBar(
         leading: IconButton(
-          // onPressed: () {
-          //   Navigator.pop(context);
-          // },
           onPressed: () => context.pop(),
-
           icon: const Icon(
             Icons.arrow_back_ios_new,
             size: 20,
@@ -25,42 +49,47 @@ class CreateShipmentScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Create Shipment",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Create Shipment",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24.0),
-                const InputTextField(
-                  hintText: "Tracking number",
-                ),
-                const SizedBox(height: 24.0),
-                const InputTextField(
-                  suffixIcon: Icon(Icons.expand_more),
-                  hintText: "Select address",
-                ),
-                const SizedBox(height: 24.0),
-                const InputTextField(
-                  hintText: "Describe product",
-                ),
-                const SizedBox(height: 24),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  // height: 50,
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(15),
+                  const SizedBox(height: 24.0),
+                  const InputTextField(
+                    hintText: "Tracking number",
                   ),
-                )
-              ],
+                  const SizedBox(height: 24.0),
+                  const InputTextField(
+                    suffixIcon: Icon(Icons.expand_more),
+                    hintText: "Select address",
+                  ),
+                  const SizedBox(height: 24.0),
+                  const InputTextField(
+                    hintText: "Describe product",
+                  ),
+                  const SizedBox(height: 24),
+                  Flexible(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      // height: 50,
+                      // height: 200,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
