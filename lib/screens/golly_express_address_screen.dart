585ab11a4c91188c93e478377bba6193be116ecf
sparkwud <kelvinagbenyo@gmail.com>
2023-10-05 15:34:07 +0000

    use custom button istead

--- lib/screens/golly_express_address_screen.dart
+++ lib/screens/golly_express_address_screen.dart
@@ -13,28 +13,6 @@ class GollyExpressAddressScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           const Divider(color: Color(0xFFEDEFEE)),
-          // Padding(
-          //   padding: const EdgeInsets.only(
-          //     left: 16,
-          //     right: 16,
-          //     top: 16,
-          //     bottom: 24.0,
-          //   ),
-          //   child: FilledButton(
-          //     style: FilledButton.styleFrom(
-          //       minimumSize: const Size(double.infinity, 54),
-          //       maximumSize: const Size(double.infinity, 54),
-          //       shape: const RoundedRectangleBorder(
-          //         borderRadius: BorderRadius.all(Radius.circular(8)),
-          //       ),
-          //     ),
-          //     onPressed: () {},
-          //     child: const Text(
-          //       "Save Changes",
-          //       style: TextStyle(fontSize: 14),
-          //     ),
-          //   ),
-          // ),
           CustomButton(
             buttonText: "Save Changes",
             isEnabled: true,
@@ -44,9 +22,6 @@ class GollyExpressAddressScreen extends StatelessWidget {
       ),
       appBar: AppBar(
         leading: IconButton(
-          // (){}: () {
-          //   Navigator.pop(context);
-          // },
           // onPressed: () => context.go("/"),
           onPressed: () => context.pop(),
           icon: const Icon(
