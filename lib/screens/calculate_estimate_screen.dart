c796be7d440e6115dfccca43922cd1f8961cb913
sparkwud <kelvinagbenyo@gmail.com>
2023-10-07 20:06:41 +0000

    add custom botton

--- lib/screens/calculate_estimate_screen.dart
+++ lib/screens/calculate_estimate_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:golly_express/components/custom_button.dart';
+import 'package:golly_express/components/product_category_bottomsheet.dart';
+import 'package:golly_express/model/my_address_container.dart';
 
 class CalculateEstimateScreen extends StatelessWidget {
   const CalculateEstimateScreen({super.key});
@@ -21,17 +24,74 @@ class CalculateEstimateScreen extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: SizedBox(
             height: MediaQuery.of(context).size.height,
-            child: const Column(
+            child: Column(
               // mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  "Calculate Estimate",
+                Container(
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFE6EDB7),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Column(
+                    children: [
+                      Text(
+                        "The amount you will pay for this",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFA3ADAA),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        "GHS 0.00",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xFFA3ADAA),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        "this includes shipping and taxes",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFA3ADAA),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  "Get Estimate ",
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.w800,
                   ),
                 ),
+                const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const ProductCategoryBottomSheet();
+                      },
+                    );
+                  },
+                  child: const MyAddressContainer(
+                    showExpandMore: true,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                CustomButton(
+                  buttonText: "Calculate",
+                  onPressed: () {},
+                )
               ],
             ),
           ),
