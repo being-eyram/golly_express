3821451cbf6de0dad4d39c0eecb3bc2833901b71
sparkwud <kelvinagbenyo@gmail.com>
2023-10-07 20:04:28 +0000

    remove padding from custom botton

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -13,33 +13,25 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        left: 16,
-        right: 16,
-        top: 14,
-        bottom: 24.0,
-      ),
-      child: FilledButton(
-        onPressed: onPressed,
-        style: FilledButton.styleFrom(
-          backgroundColor:
-              isEnabled ? const Color(0xFF557A46) : const Color(0xFFE8E9E8),
-          minimumSize: const Size(double.infinity, 54),
-          maximumSize: const Size(double.infinity, 54),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(16),
-            ),
+    return FilledButton(
+      onPressed: onPressed,
+      style: FilledButton.styleFrom(
+        backgroundColor:
+            isEnabled ? const Color(0xFF557A46) : const Color(0xFFE8E9E8),
+        minimumSize: const Size(double.infinity, 54),
+        maximumSize: const Size(double.infinity, 54),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16),
           ),
         ),
-        child: Text(
-          buttonText,
-          style: TextStyle(
-              fontSize: 15,
-              color: isEnabled ? Colors.white : const Color(0xFFA3ADAA),
-              fontWeight: FontWeight.w800),
-        ),
+      ),
+      child: Text(
+        buttonText,
+        style: TextStyle(
+            fontSize: 15,
+            color: isEnabled ? Colors.white : const Color(0xFFA3ADAA),
+            fontWeight: FontWeight.w800),
       ),
     );
   }
