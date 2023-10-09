c247451273430e8607acac717460da9a560d717e
sparkwud <kelvinagbenyo@gmail.com>
2023-10-09 10:22:01 +0000

    create notification model

--- lib/model/notification.dart
+++ lib/model/notification.dart
@@ -1,13 +1,36 @@
 //  notifications model
 
-class Notification {
-  Notification({
+import 'package:golly_express/constants.dart';
+
+class Notifications {
+  Notifications({
     this.notificationTitle,
     this.notificationSubitle,
   });
 
-  final String? notificationTitle;
-  final String? notificationSubitle;
+  String? notificationTitle;
+  String? notificationSubitle;
 }
 
-var recentNotifications = [];
+Notifications notification1 = Notifications(
+  notificationTitle: sampleNotificationTitle,
+  notificationSubitle: sampleNotificationSubtitle,
+);
+Notifications notification2 = Notifications(
+  notificationTitle: sampleNotificationTitle,
+  notificationSubitle: sampleNotificationSubtitle,
+);
+Notifications notification3 = Notifications(
+  notificationTitle: sampleNotificationTitle,
+  notificationSubitle: sampleNotificationSubtitle,
+);
+Notifications notification4 = Notifications(
+  notificationTitle: sampleNotificationTitle,
+  notificationSubitle: sampleNotificationSubtitle,
+);
+var recentNotifications = [
+  notification1,
+  notification2,
+  notification3,
+  notification4,
+];
