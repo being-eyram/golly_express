66a7c7796daac65fb4f527964c9e889ab6e01bc2
sparkwud <kelvinagbenyo@gmail.com>
2023-10-10 11:45:31 +0000

    show empty packages screen if recent packages list is empty

--- lib/screens/packages_screen.dart
+++ lib/screens/packages_screen.dart
@@ -11,6 +11,20 @@ class PackageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        // primary: false,
+        centerTitle: true,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 30.0),
+          child: Text(
+            "Packages",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
@@ -18,124 +32,125 @@ class PackageScreen extends StatelessWidget {
               horizontal: 16.0,
               vertical: 32,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    "Packages",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w800,
+            child: recentPackages.isEmpty
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        trackingHistoryPackageIcon,
+                        const Text("There are no tracking history"),
+                      ],
                     ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-                const Text(
-                  "Current Shipment",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-
-                //
-                Stack(
-                  // alignment: Alignment.topRight,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Color(0xFFFAFBFB),
-                        // color: Colors.grey,
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // const SizedBox(height: 24),
+                      const Text(
+                        "Current Shipment",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      const SizedBox(height: 16.0),
+
+                      //
+                      Stack(
+                        // alignment: Alignment.topRight,
                         children: [
-                          Text(
-                            "Product Name Goes Here",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
+                          Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xFFFAFBFB),
+                              // color: Colors.grey,
                             ),
-                          ),
-                          const SizedBox(height: 7),
-                          Text(
-                            "#1234455",
-                            style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFFA3ADAA),
-                            ),
-                          ),
-                          SizedBox(height: 40.0),
-                          FilledButton(
-                            style: FilledButton.styleFrom(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 12.0,
-                                vertical: 8.0,
-                              ),
-                              foregroundColor: Color(0xFF398B79),
-                              backgroundColor: Color(0xFFE3F2EF),
-                            ),
-                            onPressed: () {},
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
+                            padding: EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text("Received in Office"),
-                                SizedBox(width: 10),
-                                Container(
-                                  alignment: Alignment.center,
-                                  // height: 16,
-                                  // width: 16,
-                                  padding: EdgeInsets.all(3),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.white,
+                                Text(
+                                  "Product Name Goes Here",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
                                   ),
-                                  child: Icon(
-                                    Icons.arrow_forward_ios,
-                                    size: 10,
+                                ),
+                                const SizedBox(height: 7),
+                                Text(
+                                  "#1234455",
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xFFA3ADAA),
+                                  ),
+                                ),
+                                SizedBox(height: 40.0),
+                                FilledButton(
+                                  style: FilledButton.styleFrom(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 12.0,
+                                      vertical: 8.0,
+                                    ),
+                                    foregroundColor: Color(0xFF398B79),
+                                    backgroundColor: Color(0xFFE3F2EF),
+                                  ),
+                                  onPressed: () {},
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text("Received in Office"),
+                                      SizedBox(width: 10),
+                                      Container(
+                                        alignment: Alignment.center,
+                                        // height: 16,
+                                        // width: 16,
+                                        padding: EdgeInsets.all(3),
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.white,
+                                        ),
+                                        child: Icon(
+                                          Icons.arrow_forward_ios,
+                                          size: 10,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ],
                             ),
                           ),
+
+                          //TODO: properly position the package icon in container and respect border radius
+                          Positioned(
+                            bottom: -60,
+                            right: -60,
+                            child: packageVector,
+                          ),
                         ],
                       ),
-                    ),
+                      const SizedBox(height: 24),
+                      // PackageTrackingHistory()
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Tracking History",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
 
-                    //TODO: properly position the package icon in container and respect border radius
-                    Positioned(
-                      bottom: -60,
-                      right: -60,
-                      child: packageVector,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                // PackageTrackingHistory()
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Tracking History",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
+                          // wooowwwww
+                          ...recentPackages
+                              .map((package) => PackageCard(package: package))
+                        ],
                       ),
-                    ),
-                    const SizedBox(height: 16),
-
-                    // wooowwwww
-                    ...recentPackages
-                        .map((package) => PackageCard(package: package))
-                  ],
-                ),
-              ],
-            ),
+                    ],
+                  ),
           ),
         ),
       ),
