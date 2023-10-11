6445f14f6d369902fa47095bf4639db0fcb82537
sparkwud <kelvinagbenyo@gmail.com>
2023-10-11 18:48:25 +0000

    refactor package tracking details to a new file

--- lib/screens/current_shipment_screen.dart
+++ lib/screens/current_shipment_screen.dart
@@ -127,31 +127,104 @@ class PackageTrackingDetails extends StatelessWidget {
         ),
         const SizedBox(height: 16),
         Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 20,
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 20,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+              color: const Color(0xFFEDEFEE),
+              width: 1.5,
             ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                color: const Color(0xFFEDEFEE),
-                width: 1,
+          ),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // implement uisng vertical stepper
+              Text(
+                "August 4, 2023",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // implement uisng vertical stepper
-                Text(
-                  "August 4, 2023",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
+              SizedBox(height: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '6:00 PM ',
+                    style: TextStyle(
+                      color: Color(0xFFA3ADAA),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                )
-              ],
-            )),
+                  SizedBox(height: 8),
+                  Text(
+                    'Received In Office',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '6:30 PM ',
+                    style: TextStyle(
+                      color: Color(0xFFA3ADAA),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Invoice Created',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '6:35 PM ',
+                    style: TextStyle(
+                      color: Color(0xFFA3ADAA),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Approved',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
