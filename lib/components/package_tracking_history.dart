d58a54ecec64e0722cd103e382dd76e967ad30ab
sparkwud <kelvinagbenyo@gmail.com>
2023-10-11 17:52:24 +0000

    use listview.seperated in place of map

--- lib/components/package_tracking_history.dart
+++ lib/components/package_tracking_history.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:golly_express/cards/package_card.dart';
 import 'package:golly_express/constants.dart';
 import 'package:golly_express/model/package.dart';
@@ -10,25 +11,37 @@ class PackageTrackingHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Tracking History",
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Tracking History",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
-        const SizedBox(height: 16),
-        ...recentPackages.map((package) => PackageCard(package: package)),
-        // ListView.builder(
-        //     shrinkWrap: true,
-        //     itemCount: recentPackages.length,
-        //     itemBuilder: (context, index) {
-        //       return PackageCard(package: recentPackages[index]);
-        //     }),
-      ],
+          const SizedBox(height: 16),
+          // ...recentPackages.map((package) => PackageCard(package: package)),
+          ListView.separated(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: recentPackages.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () => context.push("/currentShipment"),
+                child: PackageCard(
+                  package: recentPackages[index],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 16);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
