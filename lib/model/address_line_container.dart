efb34a38c81b7c220ddfc7e7cca63e23fa227c83
sparkwud <kelvinagbenyo@gmail.com>
2023-10-12 13:23:36 +0000

    implement copied to clipboard snackbar address ontap

--- lib/model/address_line_container.dart
+++ lib/model/address_line_container.dart
@@ -12,57 +12,61 @@ class AddressLineContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(8),
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            width: 1.5,
-            color: const Color(0xFFEDEFEE),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-
-              // address lines
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                    color: Color(0xFFA3ADAA),
-                    fontSize: 13,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  children: [
-                    Text(
-                      subtitle,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // const Spacer(),
-            const Icon(
-              Icons.copy,
-              size: 20,
-            ),
-          ],
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          width: 1.5,
+          color: const Color(0xFFEDEFEE),
         ),
       ),
-      onTap: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            // address lines
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Color(0xFFA3ADAA),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Text(
+                    subtitle,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          // const Spacer(),
+          InkWell(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text("Copied to Clipboard"),
+                duration: Duration(milliseconds: 200),
+              ));
+            },
+            child: const Icon(
+              Icons.copy,
+              size: 18,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
