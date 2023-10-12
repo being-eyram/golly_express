4cfd58192b3ef5cca04b7d4aa6bf90bf2c2682b7
sparkwud <kelvinagbenyo@gmail.com>
2023-10-12 19:29:17 +0000

    attempt timeline package

--- lib/components/package_tracking_details.dart
+++ lib/components/package_tracking_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class PackageTrackingDetails extends StatelessWidget {
   const PackageTrackingDetails({super.key});
@@ -29,89 +30,15 @@ class PackageTrackingDetails extends StatelessWidget {
               width: 1.5,
             ),
           ),
-          child: const Column(
+          // child: TrackingTimeline(),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // implement uisng vertical stepper
-              Text(
-                "August 4, 2023",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '6:00 PM ',
-                    style: TextStyle(
-                      color: Color(0xFFA3ADAA),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Received In Office',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '6:30 PM ',
-                    style: TextStyle(
-                      color: Color(0xFFA3ADAA),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Invoice Created',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '6:35 PM ',
-                    style: TextStyle(
-                      color: Color(0xFFA3ADAA),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Approved',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
+              TimelineNode.simple(),
+              TimelineNode.simple(),
+              TimelineNode.simple(),
+              TimelineTile(node: )
+              TimelineNode.simple(),
             ],
           ),
         ),
