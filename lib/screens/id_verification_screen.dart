f881b69279e6e46bd6f8a072f8b188bd750ff902
sparkwud <kelvinagbenyo@gmail.com>
2023-10-14 21:03:27 +0000

    rename class

--- lib/screens/id_verification_screen.dart
+++ lib/screens/id_verification_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class IDVerification extends StatelessWidget {
-  const IDVerification({super.key});
+class IDVerificationScreen extends StatelessWidget {
+  const IDVerificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
