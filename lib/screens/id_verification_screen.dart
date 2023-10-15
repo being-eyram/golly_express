32d1596d6085e4560fe389779e43b79d987fe825
sparkwud <kelvinagbenyo@gmail.com>
2023-10-15 14:38:26 +0000

    replace bottom nav bar button with custom button

--- lib/screens/id_verification_screen.dart
+++ lib/screens/id_verification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:golly_express/components/custom_button.dart';
 
 class IDVerificationScreen extends StatelessWidget {
   const IDVerificationScreen({super.key});
@@ -7,41 +8,27 @@ class IDVerificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(
-            color: Color(0xFFEDEFEE),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16,
-              right: 16,
-              top: 16,
-              bottom: 24.0,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 14,
+          bottom: 24.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(color: Color(0xFFEDEFEE)),
+            const SizedBox(
+              height: 6,
             ),
-            child: FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: const Color(0xFFE8E9E8),
-                minimumSize: const Size(double.infinity, 54),
-                maximumSize: const Size(double.infinity, 54),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
-                  ),
-                ),
-              ),
+            CustomButton(
+              buttonText: "Verify ID",
+              isEnabled: true,
               onPressed: () {},
-              child: const Text(
-                "Verify ID",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color(0xFFA3ADAA),
-                ),
-              ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
       appBar: AppBar(
         leading: IconButton(
@@ -56,27 +43,29 @@ class IDVerificationScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 16, vertical: 50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  textAlign: TextAlign.center,
-                  "ID Verification",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    "ID Verification",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  textAlign: TextAlign.center,
-                  "We need to verify your identity",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w800,
+                  SizedBox(height: 16.0),
+                  Text(
+                    textAlign: TextAlign.center,
+                    "We need to verify your identity",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
