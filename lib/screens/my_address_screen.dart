4b621b2d338baa1c9662d5b27587909d62014d92
sparkwud <kelvinagbenyo@gmail.com>
2023-10-15 14:37:11 +0000

    replace bottom nav bar button with custom button

--- lib/screens/my_address_screen.dart
+++ lib/screens/my_address_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:golly_express/components/custom_button.dart';
 import 'package:golly_express/components/myaddress_bottom_sheet.dart';
 import 'package:golly_express/model/address.dart';
 import 'package:golly_express/model/my_address_container.dart';
@@ -9,42 +10,25 @@ class MyAddressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(
-            color: Color(0xFFEDEFEE),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16,
-              right: 16,
-              top: 16,
-              bottom: 24.0,
-            ),
-            child: FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: const Color(0xFFE8E9E8),
-                minimumSize: const Size(double.infinity, 54),
-                maximumSize: const Size(double.infinity, 54),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
-                  ),
-                ),
-              ),
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 14,
+          bottom: 24.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(color: Color(0xFFEDEFEE)),
+            const SizedBox(height: 6),
+            CustomButton(
+              buttonText: "Add Address",
               onPressed: () {},
-              child: const Text(
-                "Add Address",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color(0xFFA3ADAA),
-                ),
-              ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
       appBar: AppBar(
         leading: IconButton(
