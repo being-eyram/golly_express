1be5c69d5312c7d7534235be03e729a715181a7f
sparkwud <kelvinagbenyo@gmail.com>
2023-10-15 18:08:25 +0000

    rename classes

---
+++ lib/screens/profile screen/id verification/id_back.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:golly_express/components/custom_button.dart';
+import 'package:golly_express/constants.dart';
+
+class BackIDScreen extends StatelessWidget {
+  const BackIDScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 14,
+          bottom: 24.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(color: Color(0xFFEDEFEE)),
+            const SizedBox(
+              height: 6,
+            ),
+            CustomButton(
+              buttonText: "Open Camera",
+              isEnabled: true,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 20,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  textAlign: TextAlign.center,
+                  "Take a picture of the back of your Ghana Card",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                const Text(
+                  textAlign: TextAlign.center,
+                  "Make sure your Ghana card is readbale and well lit. No photochopies",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 70.0),
+                Center(
+                  child: backID,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
