1f3ea8083e709e0139a146dea9e22c967b7e4bc9
sparkwud <kelvinagbenyo@gmail.com>
2023-10-19 20:11:05 +0000

    whew.. finally clipped the corners of positioned package image in stack

--- lib/screens/packages_screen.dart
+++ lib/screens/packages_screen.dart
@@ -56,79 +56,83 @@ class PackageScreen extends StatelessWidget {
                       const SizedBox(height: 16.0),
 
                       //
-                      Stack(
-                        // alignment: Alignment.topRight,
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              color: Color(0xFFFAFBFB),
-                              // color: Colors.grey,
-                            ),
-                            padding: EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Product Name Goes Here",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                const SizedBox(height: 7),
-                                Text(
-                                  "#1234455",
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w700,
-                                    color: Color(0xFFA3ADAA),
-                                  ),
-                                ),
-                                SizedBox(height: 40.0),
-                                FilledButton(
-                                  style: FilledButton.styleFrom(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 12.0,
-                                      vertical: 8.0,
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Stack(
+                          clipBehavior: Clip.antiAlias,
+                          // alignment: Alignment.topRight,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Color(0xFFFAFBFB),
+                                // color: Colors.grey,
+                              ),
+                              padding: EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Product Name Goes Here",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
                                     ),
-                                    foregroundColor: Color(0xFF398B79),
-                                    backgroundColor: Color(0xFFE3F2EF),
                                   ),
-                                  onPressed: () {},
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text("Received in Office"),
-                                      SizedBox(width: 10),
-                                      Container(
-                                        alignment: Alignment.center,
-                                        // height: 16,
-                                        // width: 16,
-                                        padding: EdgeInsets.all(3),
-                                        decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.white,
-                                        ),
-                                        child: Icon(
-                                          Icons.arrow_forward_ios,
-                                          size: 10,
-                                        ),
+                                  const SizedBox(height: 7),
+                                  Text(
+                                    "#1234455",
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w700,
+                                      color: Color(0xFFA3ADAA),
+                                    ),
+                                  ),
+                                  SizedBox(height: 40.0),
+                                  FilledButton(
+                                    style: FilledButton.styleFrom(
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 12.0,
+                                        vertical: 8.0,
                                       ),
-                                    ],
+                                      foregroundColor: Color(0xFF398B79),
+                                      backgroundColor: Color(0xFFE3F2EF),
+                                    ),
+                                    onPressed: () {},
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text("Received in Office"),
+                                        SizedBox(width: 10),
+                                        Container(
+                                          padding: EdgeInsets.all(3),
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                          ),
+                                          child: Icon(
+                                            Icons.arrow_forward_ios,
+                                            size: 10,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-
-                          //TODO: properly position the package icon in container and respect border radius
-                          Positioned(
-                            bottom: -60,
-                            right: -60,
-                            child: packageVector,
-                          ),
-                        ],
+                            Positioned(
+                              bottom: -61,
+                              right: -59,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(20),
+                                ),
+                                child: packageVector,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       const SizedBox(height: 24),
                       PackageTrackingHistory()
