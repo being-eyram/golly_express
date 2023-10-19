9d04a314386fc7f1c6a809fc9aff95506891055c
sparkwud <kelvinagbenyo@gmail.com>
2023-10-19 22:03:34 +0000

    show golly express addresses on tap

--- lib/components/address_dropdown.dart
+++ lib/components/address_dropdown.dart
@@ -1,34 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:golly_express/constants.dart';
+import 'package:golly_express/model/address_line_container.dart';
 
 class AddressDropdown extends StatelessWidget {
   const AddressDropdown({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        border: Border.all(
-          width: 1.6,
-          color: const Color(0xFFEDEFEE),
+    return const ExpansionTile(
+      title: Text(
+        "golly express address",
+      ),
+      children: [
+        // Container(
+        //   padding: const EdgeInsets.all(16),
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.circular(15),
+        //     border: Border.all(
+        //       width: 1.6,
+        //       color: const Color(0xFFEDEFEE),
+        //     ),
+        //   ),
+        //   child: Row(
+        //     children: [
+        //       gollyExpressLogoMini,
+        //       const SizedBox(width: 8),
+        //       const Text(
+        //         "Your Golly Express Address",
+        //         style: TextStyle(
+        //           fontWeight: FontWeight.w700,
+        //         ),
+        //       ),
+        //       const Spacer(),
+        //       const Icon(Icons.expand_more)
+        //     ],
+        //   ),
+        // ),
+        AddressLineContainer(
+          title: "Address Line 1",
+          subtitle: "2507 Investors Row, STE 100 Unit D5",
         ),
-      ),
-      child: Row(
-        children: [
-          gollyExpressLogoMini,
-          const SizedBox(width: 8),
-          const Text(
-            "Your Golly Express Address",
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          const Spacer(),
-          const Icon(Icons.expand_more)
-        ],
-      ),
+        SizedBox(height: 16),
+        AddressLineContainer(
+          title: "Address Line 2",
+          subtitle: "1904",
+        ),
+        SizedBox(height: 16),
+        AddressLineContainer(
+          title: "City",
+          subtitle: "Orlando",
+        ),
+        SizedBox(height: 16),
+        AddressLineContainer(
+          title: "Zip Code",
+          subtitle: "32837",
+        ),
+        SizedBox(height: 16),
+        AddressLineContainer(
+          title: "Phone",
+          subtitle: "+1(689) 209-8652",
+        ),
+      ],
     );
   }
 }
