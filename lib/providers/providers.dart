968a08307596d3e03bc2c45e4da411fc5084553d
sparkwud <kelvinagbenyo@gmail.com>
2023-10-25 22:07:33 +0000

    extract onboarding providers from (general) providers file

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,28 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:golly_express/api/services/authentication_service.dart';
 
-final onboardingCaptionProvider = Provider<List<String>>(
-  (_) => [
-    "Get your package with our shipping address",
-    "Package processing and notifications",
-    "Product tracking and delivery"
-  ],
-);
-
-final onboardingDescriptionProvider = Provider<List<String>>(
-  (_) => [
-    "Shop online for your favorites products and use our US shipping address at checkout for free domestic shipping offers",
-    "We'll notify you when your package arrives at our US address and process your shipment.",
-    "Track your shipment and receive your package in Ghana without stress."
-  ],
-);
-
 final authProvider =
     StateNotifierProvider<AuthenticationService, AuthenticationState>(
         (ref) => AuthenticationService());
 
 final showPasswordProvider = StateProvider<bool>((_) => false);
-final onboardingPageIndexProvider = StateProvider<int>((_) => 0);
 
 final usernameProvider = Provider<String>(
   (_) => "Ben-Aaron Mills-Pappoe",
