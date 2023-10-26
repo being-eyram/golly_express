7e44545d0f95ea1864ad9fe96dfdacb4a87347c6
sparkwud <kelvinagbenyo@gmail.com>
2023-10-26 11:56:37 +0000

    change password input default text obscure state

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -5,17 +5,17 @@ final authProvider =
     StateNotifierProvider<AuthenticationService, AuthenticationState>(
         (ref) => AuthenticationService());
 
-final showPasswordProvider = StateProvider<bool>((_) => false);
+final showPasswordProvider = StateProvider<bool>((_) => true);
 
 final usernameProvider = Provider<String>(
   (_) => "Ben-Aaron Mills-Pappoe",
 );
 
 Future<String> getProfileUsername(name) async {
-  await Future.delayed(const Duration(seconds: 7));
+  await Future.delayed(const Duration(seconds: 5));
   return name;
 }
 
 final profileNameProvider = FutureProvider<String>(
-  (_) => getProfileUsername("kisa"),
+  (_) => getProfileUsername("Ben-Aaron Mills-Pappoe"),
 );
