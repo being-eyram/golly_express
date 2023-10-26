ca572df54c50638a14203160a2afac0e12ffc30a
sparkwud <kelvinagbenyo@gmail.com>
2023-10-26 13:18:54 +0000

    create add address onboarding screen

---
+++ lib/screens/onboarding/add_address.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:golly_express/components/custom_button.dart';
+import 'package:golly_express/constants.dart';
+
+class AddAddressScreen extends ConsumerWidget {
+  const AddAddressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(
+            color: Color(0xFFEDEFEE),
+            height: 0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 16, right: 16, top: 14, bottom: 24.0),
+            child: CustomButton(
+              borderRadius: 8,
+              isEnabled: true,
+              buttonText: "Complete",
+              onPressed: () => context.go("/mainContainer"),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 64, right: 16, left: 16),
+          child: Center(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                gollyExpressLogo,
+                const Text(
+                  "Add Address",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  "Add address where packages will delivered to you",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
