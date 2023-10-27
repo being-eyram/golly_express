9f58e0f86cba7c39e44264d00e55eb9c83858934
sparkwud <kelvinagbenyo@gmail.com>
2023-10-27 13:59:40 +0000

    add login request model

---
+++ lib/api/request_models/login_models.dart
@@ -0,0 +1,35 @@
+import 'package:golly_express/api/helper/serializable.dart';
+
+class LoginRequestModel implements Serializable {
+  LoginRequestModel({
+    required this.email,
+    required this.password,
+  });
+
+  final String email;
+  final String password;
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "email": email,
+      "password": password,
+    };
+  }
+}
+
+class LoginResponseModel {
+  LoginResponseModel({
+    required this.token,
+    required this.error,
+  });
+
+  final String token;
+  final String error;
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) {
+    return LoginResponseModel(
+      token: json["token"] ?? "",
+      error: json["error"] ?? "",
+    );
+  }
+}
