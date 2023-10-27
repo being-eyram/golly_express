750349692c141486659a7243e9b9b31ef0db7ba5
sparkwud <kelvinagbenyo@gmail.com>
2023-10-27 13:59:50 +0000

    create sign up model

---
+++ lib/api/request_models/signup_models.dart
@@ -0,0 +1,25 @@
+import 'package:golly_express/api/helper/serializable.dart';
+
+class SignUpRequestModel implements Serializable {
+  SignUpRequestModel({
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    required this.fullName,
+  });
+
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final String fullName;
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'password': password,
+      'phoneNumber': phoneNumber,
+      'Fullname': fullName,
+    };
+  }
+}
