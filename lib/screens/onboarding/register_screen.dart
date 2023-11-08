b19073f4e965f50c9e9efb00e6a4304255490920
sparkwud <kelvinagbenyo@gmail.com>
2023-11-08 13:04:48 +0000

    fix layout and overflow issues

--- lib/screens/onboarding/register_screen.dart
+++ lib/screens/onboarding/register_screen.dart
@@ -34,105 +34,115 @@ class RegisterScreen extends ConsumerWidget {
     }
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(
-            color: Color(0xFFEDEFEE),
-            height: 0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 16, right: 16, top: 14, bottom: 24.0),
-            child: CustomButton(
-              borderRadius: 8,
-              isEnabled: true,
-              buttonText: "Next",
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  // final email = emailController.text;
-                  // print("Valid Email");
-                  context.go("/registerPassword");
-                }
-              },
+      body: Scaffold(
+        backgroundColor: Colors.white,
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(
+              color: Color(0xFFEDEFEE),
+              height: 0,
             ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 64, right: 16, left: 16),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                gollyExpressLogo,
-                const Text(
-                  "Register to start",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  "Enter your information below",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                ),
-                const SizedBox(height: 40),
-
-                // email address
-                InputTextField(
-                  // autovalidateMode: AutovalidateMode.onUserInteraction,
-                  keyboardType: TextInputType.emailAddress,
-                  initialValue: email,
-                  // controller: emailController,
-                  hintText: "Enter Email",
-                  validator: (input) {
-                    return validateEmail(input);
-                  },
-                ),
-                const SizedBox(height: 20),
-
-                // full name
-                InputTextField(
-                  // autovalidateMode: AutovalidateMode.onUserInteraction,
-                  hintText: "Full Name",
-                  controller: fullNameController,
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return "Enter your full name";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
-
-                // phone number
-                InputTextField(
-                  controller: phoneNumberController,
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return "Enter your phone number";
-                    }
-                    return null;
-                  },
-                  hintText: "Phone Number",
-                  prefixIcon: const Padding(
-                    padding: EdgeInsets.only(
-                        top: 15, bottom: 15, left: 15, right: 10),
-                    child: Text(
-                      "+233 ",
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, top: 14, bottom: 24.0),
+              child: CustomButton(
+                borderRadius: 8,
+                isEnabled: true,
+                buttonText: "Next",
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    // final email = emailController.text;
+                    // print("Valid Email");
+                    context.go("/registerPassword");
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    gollyExpressLogo,
+                    const Text(
+                      "Register to start",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 24,
                         fontWeight: FontWeight.w800,
                       ),
                     ),
-                  ),
-                  keyboardType: TextInputType.phone,
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Enter your information below",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // email address
+                    InputTextField(
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.emailAddress,
+                      initialValue: email,
+                      // controller: emailController,
+                      hintText: "Enter Email",
+                      validator: (input) {
+                        return validateEmail(input);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+
+                    // full name
+                    InputTextField(
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      hintText: "Full Name",
+                      controller: fullNameController,
+                      validator: (input) {
+                        if (input.isEmpty) {
+                          return "Enter your full name";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+
+                    // phone number
+                    InputTextField(
+                      controller: phoneNumberController,
+                      validator: (input) {
+                        if (input.isEmpty) {
+                          return "Enter your phone number";
+                        }
+                        return null;
+                      },
+                      hintText: "Phone Number",
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.only(
+                            top: 15, bottom: 15, left: 15, right: 10),
+                        child: Text(
+                          "+233 ",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.phone,
+                    ),
+                    const SizedBox(height: 16),
+                  ],
                 ),
-                const SizedBox(height: 16),
-              ],
+              ),
             ),
           ),
         ),
