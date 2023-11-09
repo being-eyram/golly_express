60906c4f1314f91645c2df0656ec29fc66d15c18
sparkwud <kelvinagbenyo@gmail.com>
2023-11-09 18:34:40 +0000

    update strinf extensions with toTitleCase

--- lib/extensions/string_extensions.dart
+++ lib/extensions/string_extensions.dart
@@ -5,3 +5,12 @@ extension EmailValidator on String {
         .hasMatch(this);
   }
 }
+
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
+      .split(' ')
+      .map((str) => str.toCapitalized())
+      .join(' ');
+}
