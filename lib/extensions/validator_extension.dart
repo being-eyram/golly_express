db1d0b26dcf6f5366437f6ef47811cd3fc38d0a2
sparkwud <kelvinagbenyo@gmail.com>
2023-11-10 15:31:39 +0000

    update validator string extensions

--- lib/extensions/validator_extension.dart
+++ lib/extensions/validator_extension.dart
@@ -7,29 +7,29 @@ extension FieldValidator on String {
       RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,10}$')
           .hasMatch(this);
 
-  bool get isValidName {
+  bool isValidName() {
     final nameRegExp =
         RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
     return nameRegExp.hasMatch(this);
   }
 
-  bool get isValidPassword {
+  bool isValidPassword() {
     final passwordRegExp = RegExp(
         r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\><*~]).{8,}/pre>');
     return passwordRegExp.hasMatch(this);
   }
 
-  bool get isValidEmail {
+  bool isValidEmail() {
     final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
     return emailRegExp.hasMatch(this);
   }
 
-  bool get isValidPhoneNumber {
+  bool isValidPhoneNumber() {
     final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
     return phoneRegExp.hasMatch(this);
   }
 
-  bool get isNotNull {
+  bool isNotNull() {
     return this != null;
   }
 }
