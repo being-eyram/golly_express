a5703c6459776fa564d11f9e7aab52a560aa7cae
sparkwud <kelvinagbenyo@gmail.com>
2023-11-13 16:56:35 +0000

    dummy login function

--- lib/api/services/authentication_service.dart
+++ lib/api/services/authentication_service.dart
@@ -51,3 +51,30 @@ class AuthenticationService extends StateNotifier<AuthenticationState> {
 final authenticationServiceProvider = Provider<AuthenticationService>(
   (_) => AuthenticationService(),
 );
+
+
+// void login(String email , password) async {
+  
+//   try{
+    
+//     Response response = await post(
+//       Uri.parse('https://reqres.in/api/login'),
+//       body: {
+//         'email' : email,
+//         'password' : password
+//       }
+//     );
+
+//     if(response.statusCode == 200){
+      
+//       var data = jsonDecode(response.body.toString());
+//       print(data['token']);
+//       print('Login successfully');
+
+//     }else {
+//       print('failed');
+//     }
+//   }catch(e){
+//     print(e.toString());
+//   }
+// }
