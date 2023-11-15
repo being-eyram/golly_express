09bf2775898843a91b23de813aac0d019cf537f1
sparkwud <kelvinagbenyo@gmail.com>
2023-11-15 14:56:18 +0000

    remove validation logic to new file

--- lib/components/input_field.dart
+++ lib/components/input_field.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:golly_express/providers/onboarding_providers.dart';
 
 final showPasswordProvider = StateProvider<bool>((_) => true);
 
