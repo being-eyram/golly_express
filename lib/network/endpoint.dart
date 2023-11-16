6492a96ee1ca162dea0e0823a100a33143e896bf
sparkwud <kelvinagbenyo@gmail.com>
2023-11-16 13:06:19 +0000

    update endpoints

--- lib/network/endpoint.dart
+++ lib/network/endpoint.dart
@@ -5,4 +5,11 @@ class Endpoint {
   static const _baseUrl = 'gollyexpress.com';
   static final login = Uri.https(_baseUrl, '/login');
   static final signup = Uri.https(_baseUrl, '/signup');
+  static final packageCategories = Uri.https(_baseUrl, '/categories');
+  static final forgotPassword = Uri.https(_baseUrl, '/forgot-password');
+  static final verifyOtp = Uri.https(_baseUrl, '/forgot-password/verify');
+  static final resetPassword = Uri.https(_baseUrl, '/forgot-password/reset');
+  static final address = Uri.https(_baseUrl, '/address');
+  static final user = Uri.https(_baseUrl, '/user');
+  static final categories = Uri.https(_baseUrl, '/estimate/categories');
 }
