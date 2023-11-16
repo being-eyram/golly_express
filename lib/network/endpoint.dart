3ab8b7ee87b490b45bf6ca3788319b63eb3c87ee
sparkwud <kelvinagbenyo@gmail.com>
2023-11-16 15:20:04 +0000

    change class name

--- lib/network/endpoint.dart
+++ lib/network/endpoint.dart
@@ -1,11 +1,11 @@
-/// The [Endpoint] class provides a centralized management of API endpoints for the app.
-class Endpoint {
-  Endpoint._();
+/// The [Endpoints] class provides a centralized management of API endpoints for the app.
+class Endpoints {
+  Endpoints._();
 
   static const _baseUrl = 'gollyexpress.com';
   static final login = Uri.https(_baseUrl, '/login');
   static final signup = Uri.https(_baseUrl, '/signup');
-  static final packageCategories = Uri.https(_baseUrl, '/categories');
+  static final packageCategories = Uri.https(_baseUrl, '/estimate/categories');
   static final forgotPassword = Uri.https(_baseUrl, '/forgot-password');
   static final verifyOtp = Uri.https(_baseUrl, '/forgot-password/verify');
   static final resetPassword = Uri.https(_baseUrl, '/forgot-password/reset');
