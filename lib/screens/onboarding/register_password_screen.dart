bc22979d981cf8e17c31c9d4b3f9235542546a14
sparkwud <kelvinagbenyo@gmail.com>
2023-11-16 20:37:14 +0000

    add back button to appbar

--- lib/screens/onboarding/register_password_screen.dart
+++ lib/screens/onboarding/register_password_screen.dart
@@ -47,6 +47,15 @@ class RegisterPasswordScreen extends ConsumerWidget {
     });
 
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 20,
+          ),
+        ),
+      ),
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
       bottomNavigationBar: Column(
