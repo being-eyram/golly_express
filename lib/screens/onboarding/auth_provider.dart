99113dcddefb9520aa12d09bbb3265afbd12cec1
sparkwud <kelvinagbenyo@gmail.com>
2023-11-16 15:23:54 +0000

    fix import issues

--- lib/screens/onboarding/auth_provider.dart
+++ lib/screens/onboarding/auth_provider.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:golly_express/network/api/request_models/login_models.dart';
-import 'package:golly_express/network/api/request_models/signup_models.dart';
+import 'package:golly_express/network/api/request_models/login_request_models.dart';
+import 'package:golly_express/network/api/request_models/signup_request_models.dart';
 import 'package:golly_express/network/api/response_models/auth_response.dart';
 import 'package:golly_express/network/api/services/api_service.dart';
 
