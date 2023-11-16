0e099f55311b829fc703fffaf9b1dfdcf220cf11
sparkwud <kelvinagbenyo@gmail.com>
2023-11-16 14:54:36 +0000

    replace login response with auth response

---
+++ lib/network/api/response_models/auth_response.dart
@@ -0,0 +1,41 @@
+class AuthResponse {
+  final Token token;
+  final String message;
+  final String status;
+
+  AuthResponse({
+    required this.token,
+    required this.message,
+    required this.status,
+  });
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      token: Token.fromJson(json['data']),
+      message: json['message'],
+      status: json['status'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'data': token.toJson(),
+      'message': message,
+      'status': status,
+    };
+  }
+}
+
+class Token {
+  final String token;
+
+  Token({required this.token});
+
+  factory Token.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return Token(token: json['token']);
+  }
+
+  Map<String, dynamic> toJson() => {'token': token};
+}
