a2abf72a90c9081c38c07e3d64fcca2523ae393f
sparkwud <kelvinagbenyo@gmail.com>
2023-11-17 15:36:52 +0000

    update shared preferences with user bearer token

--- lib/shared_prefs/shared_prefs.dart
+++ lib/shared_prefs/shared_prefs.dart
@@ -9,3 +9,13 @@ Future<bool?> getUserOnboardStatus() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   return prefs.getBool('is_onboarded');
 }
+
+setUserBearerToken(String value) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('bearer_token', value);
+}
+
+Future<String?> getUserBearerToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('bearer_token');
+}
