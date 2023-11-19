577844fcdced91c1075fd65625a4f17c53fc3566
sparkwud <kelvinagbenyo@gmail.com>
2023-11-19 15:59:24 +0000

    create otp fields with custom functionalities

--- lib/screens/onboarding/otp_screen.dart
+++ lib/screens/onboarding/otp_screen.dart
@@ -1,138 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:golly_express/components/custom_button.dart';
-import 'package:golly_express/providers/onboarding_providers.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OtpScreen extends ConsumerWidget {
   const OtpScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Divider(color: Color(0xFFEDEFEE)),
-          const SizedBox(
-            height: 6,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            child: CustomButton(
-              borderRadius: 8,
-              buttonText: "Next",
-              onPressed: () {},
+      body: Scaffold(
+        bottomNavigationBar: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(color: Color(0xFFEDEFEE)),
+            const SizedBox(
+              height: 6,
             ),
-          )
-        ],
-      ),
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => context.pop(),
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            size: 20,
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: 24.0,
+              ),
+              child: CustomButton(
+                borderRadius: 8,
+                buttonText: "Verify",
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => context.pop(),
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 20,
+            ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Forgot Password",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                const Text(
-                  "Select which contact details should we use to reset your password",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(),
-                ),
-                const SizedBox(height: 32),
-                InkWell(
-                  onTap: () {},
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(
-                        color: const Color(0xFFEDEFEE),
-                        width: 1.5,
-                      ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 54),
+            child: Form(
+              key: formKey,
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Forgot Password",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
                     ),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Send OTP via SMS",
-                          style: TextStyle(
-                            color: Color(0xFFA3ADAA),
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "+233 57 159 2866",
+                  ),
+                  const SizedBox(height: 8.0),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontFamily: GoogleFonts.dmSans().fontFamily,
+                        color: Colors.black,
+                      ),
+                      children: const <TextSpan>[
+                        TextSpan(
+                          text:
+                              'A SMS with the verifcation PIN has been sent to ',
                           style: TextStyle(),
+                        ),
+                        TextSpan(
+                          text: '+233 57 159 2866',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                InkWell(
-                  onTap: () {
-                    ref
-                        .read(emailOptionSelected.notifier)
-                        .update((state) => !state);
-                  },
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(
-                        color: ref.watch(smsOptionSelected)
-                            ? const Color(0xFF557A46)
-                            : const Color(0xFFEDEFEE),
-                        width: 1.5,
+                  const SizedBox(height: 32),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OtpField(first: true),
+                      OtpField(),
+                      OtpField(),
+                      OtpField(
+                        last: true,
                       ),
-                    ),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Send OTP via Email",
-                          style: TextStyle(
-                            color: Color(0xFFA3ADAA),
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "benaaron866@gmail.com",
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontFamily: GoogleFonts.dmSans().fontFamily,
+                        color: Colors.black,
+                      ),
+                      children: const <TextSpan>[
+                        TextSpan(
+                          text: 'Didn’t receive it? ',
                           style: TextStyle(),
+                        ),
+                        TextSpan(
+                          text: 'Resend Code',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF557A46),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+}
+
+class OtpField extends StatelessWidget {
+  const OtpField({
+    super.key,
+    this.first = false,
+    this.last = false,
+  });
+  final bool? first;
+  final bool? last;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height / 14,
+      width: MediaQuery.of(context).size.width / 8,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: "*",
+          border: InputBorder.none,
+          // enabledBorder: outlineBorderPropGenerator(const Color(0xFFEDEFEE)),
+          enabledBorder: outlineBorderPropGenerator(const Color(0x1F000000)),
+          focusedBorder: outlineBorderPropGenerator(const Color(0xFF557A46)),
+        ),
+        style: const TextStyle(fontSize: 16),
+        textAlign: TextAlign.center,
+        // textInputAction: TextInputAction.previous,
+        keyboardType: TextInputType.number,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly,
+        ],
+        onChanged: (value) {
+          if (value.length == 1 && last == false) {
+            FocusScope.of(context).nextFocus();
+          }
+          if (value.isEmpty && first == false) {
+            FocusScope.of(context).previousFocus();
+          }
+          // } else {
+          //   FocusScope.of(context).previousFocus();
+          // }
+        },
+      ),
+    );
+  }
+}
+
+outlineBorderPropGenerator(Color color, {double width = 1.5}) {
+  return OutlineInputBorder(
+    borderRadius: const BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(width: width, color: color),
+  );
 }
