d7fcb3d90c73a94c30363f5e7348d82a4da3d017
sparkwud <kelvinagbenyo@gmail.com>
2023-11-20 15:02:50 +0000

    fix bottom overflow caused by keyboard

--- lib/screens/create_shipment_screen.dart
+++ lib/screens/create_shipment_screen.dart
@@ -18,6 +18,7 @@ class _CreateShipmentScreenState extends State<CreateShipmentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.only(
           left: 16,
