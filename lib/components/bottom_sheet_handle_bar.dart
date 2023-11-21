8ad103e65a5989671532e54a390b018658c1f4c1
sparkwud <kelvinagbenyo@gmail.com>
2023-11-21 14:26:17 +0000

    create bottom modal sheet handle bar component

---
+++ lib/components/bottom_sheet_handle_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetHandleBar extends StatelessWidget {
+  const BottomSheetHandleBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // final theme = Theme.of(context);
+    return FractionallySizedBox(
+      widthFactor: 0.18,
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 14.0,
+        ),
+        child: Container(
+          height: 4.5,
+          decoration: BoxDecoration(
+            // color: theme.dividerColor,
+            color: const Color(0xFFB5BDBB),
+            borderRadius: BorderRadius.circular(2.5),
+          ),
+        ),
+      ),
+    );
+  }
+}
