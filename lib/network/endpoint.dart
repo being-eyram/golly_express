55deb36ec4ff480d32efb4f4087280f5c6101f8b
sparkwud <kelvinagbenyo@gmail.com>
2023-11-22 16:37:36 +0000

    fix endpoints url

--- lib/network/endpoint.dart
+++ lib/network/endpoint.dart
@@ -6,10 +6,14 @@ class Endpoints {
   static final login = Uri.https(_baseUrl, '/login');
   static final signup = Uri.https(_baseUrl, '/signup');
   static final packageCategories = Uri.https(_baseUrl, '/estimate/categories');
-  static final forgotPassword = Uri.https(_baseUrl, '/forgot-password');
+  // static final forgotPassword = Uri.https(_baseUrl, '/forgot-password');
   static final verifyOtp = Uri.https(_baseUrl, '/forgot-password/verify');
   static final resetPassword = Uri.https(_baseUrl, '/forgot-password/reset');
   static final address = Uri.https(_baseUrl, '/address');
   static final user = Uri.https(_baseUrl, '/user');
   static final categories = Uri.https(_baseUrl, '/estimate/categories');
+
+  static Uri forgotPassword({Map<String, dynamic>? queryParam}) {
+    return Uri.https(_baseUrl, '/forgot-password', queryParam);
+  }
 }
