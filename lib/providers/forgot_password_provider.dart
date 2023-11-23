fa48e686d792cf96a82b72b11e7f938dbcc86d16
sparkwud <kelvinagbenyo@gmail.com>
2023-11-23 10:35:26 +0000

    add autodispose to forgot password provider to enable  request resend

--- lib/providers/forgot_password_provider.dart
+++ lib/providers/forgot_password_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:golly_express/network/api/services/api_service.dart';
 
-final forgotPasswordProvider = FutureProvider.family((ref, String email) async {
+final forgotPasswordProvider =
+    FutureProvider.family.autoDispose((ref, String email) async {
   return ref.watch(apiServiceProvider).forgotPassword(email: email);
 });
