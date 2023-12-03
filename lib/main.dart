e3cfda40033b54c36457cc20d8517f7336c349fd
sparkwud <kelvinagbenyo@gmail.com>
2023-12-03 00:46:08 +0000

    setup and configutre screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:golly_express/navigation/router.dart';
 import 'package:golly_express/shared_prefs/shared_prefs.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,32 +27,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final baseTextTheme = Theme.of(context).textTheme;
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Golly Express',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.dmSans().fontFamily,
-        bottomSheetTheme: const BottomSheetThemeData(
-          backgroundColor: Colors.white,
-          modalBackgroundColor: Colors.white,
-        ),
-        // dividerColor: Colors.grey.shade300,
-        appBarTheme: const AppBarTheme(color: Colors.white),
-        textTheme: GoogleFonts.dmSansTextTheme(baseTextTheme),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF557A46),
-            foregroundColor: Colors.white,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Golly Express',
+          theme: ThemeData(
+            bottomSheetTheme: const BottomSheetThemeData(
+              backgroundColor: Colors.white,
+              modalBackgroundColor: Colors.white,
+            ),
+            // dividerColor: Colors.grey.shade300,
+            fontFamily: GoogleFonts.dmSans().fontFamily,
+            appBarTheme: const AppBarTheme(color: Colors.white),
+            textTheme: GoogleFonts.dmSansTextTheme(baseTextTheme),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF557A46),
+                foregroundColor: Colors.white,
+              ),
+            ),
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color(0xFF557A46),
+            ).copyWith(
+              background: Colors.white,
+            ),
           ),
-        ),
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF557A46),
-        ).copyWith(
-          background: Colors.white,
-        ),
-      ),
-      routerConfig: appRouter,
+          routerConfig: appRouter,
+        );
+      },
     );
   }
 }
