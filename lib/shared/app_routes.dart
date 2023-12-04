e6884d01bbd5b681667c41c0154cc5df64ce66a5
sparkwud <kelvinagbenyo@gmail.com>
2023-12-04 20:00:58 +0000

    create app routes class

---
+++ lib/shared/app_routes.dart
@@ -0,0 +1,47 @@
+class AppRoutes {
+  // BASE
+  static const String base = '/';
+
+  //! SETTINGS
+  static const String settings = '/settings';
+
+  // ONBOARDING ROUTES
+  static const String onboard = '/onboard';
+  static const String onboarding = '/onboarding';
+
+  static const String getStarted = '/getStarted';
+  static const String signinOption = '/signinOption';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String registerPassword = '/registerPassword';
+  static const String forgotPassword = '/forgotPassword';
+  static const String otpScreen = '/otpScreen';
+  static const String resetPassword = '/resetPassword';
+
+  static const String addAddress = '/addAddress';
+
+  // BOTTOM NAV BAR ROUTES
+  static const String home = '/home';
+  static const String mainContainer = '/mainContainer';
+  static const String packages = '/packages';
+  static const String profile = '/profile';
+  static const String notifications = '/notifications';
+
+  // PLUS BOTTOM SHEET ROUTES
+  static const String createShipment = '/createShipment';
+  static const String calculateEstimate = '/calculateEstimate';
+
+  // PROFILE SCREEN ROUTES
+  static const String editProfile = '/editProfile';
+  static const String changePassword = '/changePassword';
+  static const String currentShipment = '/currentShipment';
+  static const String gollyExpressAddress = '/gollyExpressAddress';
+  static const String myAddress = '/myAddress';
+
+  // ID VERIFICATION ROUTES
+  static const String verifyID = '/verifyID';
+  static const String frontID = '/frontID';
+  static const String backID = '/backID';
+  static const String checkPhoto = '/checkPhoto';
+  static const String verifiedSuccess = '/verifiedSuccess';
+}
