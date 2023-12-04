45a1425fd8884edf758b7e718e97c02f5cd0a112
sparkwud <kelvinagbenyo@gmail.com>
2023-12-04 20:00:39 +0000

    create app widget extensions

---
+++ lib/extensions/widget_extensions.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+extension WidgetExtensions on double {
+  Widget get sbH => SizedBox(
+        height: h,
+      );
+
+  Widget get sbW => SizedBox(
+        width: w,
+      );
+
+  EdgeInsetsGeometry get padA => EdgeInsets.all(this);
+
+  EdgeInsetsGeometry get padV => EdgeInsets.symmetric(vertical: h);
+
+  EdgeInsetsGeometry get padH => EdgeInsets.symmetric(horizontal: w);
+}
