ff6e81edb19a7df10e3e3bd68cccf227bc46d24b
sparkwud <kelvinagbenyo@gmail.com>
2023-12-14 01:13:33 +0000

    extension on BuildContext to get device properties

--- lib/shared/extensions/context_extension.dart
+++ lib/shared/extensions/context_extension.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 // e.g. context.height
 
 extension ContextExtension on BuildContext {
-  ThemeData get theme => Theme.of(this);
+  // ThemeData get theme => Theme.of(this);
 
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 
