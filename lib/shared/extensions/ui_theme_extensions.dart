6ef3dc447b44f4b6c828844a5c893215c959e6ee
sparkwud <kelvinagbenyo@gmail.com>
2023-12-15 01:10:11 +0000

    create text theme extenion on BuildContext

--- lib/shared/extensions/ui_theme_extensions.dart
+++ lib/shared/extensions/ui_theme_extensions.dart
@@ -3,34 +3,36 @@ import 'package:flutter/material.dart';
 extension UIThemeExtension on BuildContext {
   // * (default) TextTheme
 
-  TextStyle? get h1 => Theme.of(this).textTheme.displayLarge;
-  TextStyle? get h2 => Theme.of(this).textTheme.displayMedium;
-  TextStyle? get h3 => Theme.of(this).textTheme.displaySmall;
-  TextStyle? get h4 => Theme.of(this).textTheme.headlineMedium;
-  TextStyle? get h5 => Theme.of(this).textTheme.headlineSmall;
-  TextStyle? get h6 => Theme.of(this).textTheme.titleLarge;
+  ThemeData get theme => Theme.of(this);
 
-  TextStyle? get sub1 => Theme.of(this).textTheme.titleMedium;
-  TextStyle? get sub2 => Theme.of(this).textTheme.titleSmall;
+  TextStyle? get h1 => theme.textTheme.displayLarge;
+  TextStyle? get h2 => theme.textTheme.displayMedium;
+  TextStyle? get h3 => theme.textTheme.displaySmall;
+  TextStyle? get h4 => theme.textTheme.headlineMedium;
+  TextStyle? get h5 => theme.textTheme.headlineSmall;
+  TextStyle? get h6 => theme.textTheme.titleLarge;
 
-  TextStyle? get body1 => Theme.of(this).textTheme.bodyLarge;
-  TextStyle? get body2 => Theme.of(this).textTheme.bodyMedium;
+  TextStyle? get sub1 => theme.textTheme.titleMedium;
+  TextStyle? get sub2 => theme.textTheme.titleSmall;
 
-  TextStyle? get button => Theme.of(this).textTheme.labelLarge;
+  TextStyle? get body1 => theme.textTheme.bodyLarge;
+  TextStyle? get body2 => theme.textTheme.bodyMedium;
+
+  TextStyle? get button => theme.textTheme.labelLarge;
 
   // * PrimaryTextTheme
-  TextStyle? get pH1 => Theme.of(this).primaryTextTheme.displayLarge;
-  TextStyle? get pH2 => Theme.of(this).primaryTextTheme.displayMedium;
-  TextStyle? get pH3 => Theme.of(this).primaryTextTheme.displaySmall;
-  TextStyle? get pH4 => Theme.of(this).primaryTextTheme.headlineMedium;
-  TextStyle? get pH5 => Theme.of(this).primaryTextTheme.headlineSmall;
-  TextStyle? get pH6 => Theme.of(this).primaryTextTheme.titleLarge;
+  TextStyle? get pH1 => theme.primaryTextTheme.displayLarge;
+  TextStyle? get pH2 => theme.primaryTextTheme.displayMedium;
+  TextStyle? get pH3 => theme.primaryTextTheme.displaySmall;
+  TextStyle? get pH4 => theme.primaryTextTheme.headlineMedium;
+  TextStyle? get pH5 => theme.primaryTextTheme.headlineSmall;
+  TextStyle? get pH6 => theme.primaryTextTheme.titleLarge;
 
-  TextStyle? get pSub1 => Theme.of(this).primaryTextTheme.titleMedium;
-  TextStyle? get pSub2 => Theme.of(this).primaryTextTheme.titleSmall;
+  TextStyle? get pSub1 => theme.primaryTextTheme.titleMedium;
+  TextStyle? get pSub2 => theme.primaryTextTheme.titleSmall;
 
-  TextStyle? get pBody1 => Theme.of(this).primaryTextTheme.bodyLarge;
-  TextStyle? get pBody2 => Theme.of(this).primaryTextTheme.bodyMedium;
+  TextStyle? get pBody1 => theme.primaryTextTheme.bodyLarge;
+  TextStyle? get pBody2 => theme.primaryTextTheme.bodyMedium;
 
-  TextStyle? get pButton => Theme.of(this).primaryTextTheme.labelLarge;
+  TextStyle? get pButton => theme.primaryTextTheme.labelLarge;
 }
