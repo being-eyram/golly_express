314fdefacabec3f96f18315a94af06078ad960b3
sparkwud <kelvinagbenyo@gmail.com>
2023-12-15 10:04:47 +0000

    make screen adaptive

--- lib/components/address_dropdown.dart
+++ lib/components/address_dropdown.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:golly_express/constants.dart';
 import 'package:golly_express/model/address_line_container.dart';
 
@@ -10,54 +11,57 @@ class AddressDropdown extends StatelessWidget {
     return Container(
       // padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(15.r),
         border: Border.all(
           width: 1.6,
           color: const Color(0xFFEDEFEE),
         ),
       ),
       child: ExpansionTile(
-        childrenPadding: const EdgeInsets.symmetric(vertical: 2),
+        childrenPadding: EdgeInsets.all(16.h).copyWith(top: 0),
         shape: Border.all(color: Colors.transparent),
         leading: SizedBox(
           child: gollyExpressLogoMini,
         ),
+        tilePadding: EdgeInsets.symmetric(horizontal: 16.h),
         title: const Text(
           "Your Golly Express Address",
           style: TextStyle(
             fontWeight: FontWeight.w700,
           ),
         ),
-        children: const [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Divider(
-              height: 1,
-              thickness: 1.5,
-              color: Color(0xFFF6F7F6),
-            ),
+        children: [
+          const Divider(
+            height: 1,
+            thickness: 1.5,
+            color: Color(0xFFF6F7F6),
           ),
-          AddressLineContainer(
+          SizedBox(height: 12.h),
+          const AddressLineContainer(
             showBorder: false,
             title: "Address Line 1",
             subtitle: "2507 Investors Row, STE 100 Unit D5",
           ),
-          AddressLineContainer(
+          SizedBox(height: 16.h),
+          const AddressLineContainer(
             showBorder: false,
             title: "Address Line 2",
             subtitle: "1904",
           ),
-          AddressLineContainer(
+          SizedBox(height: 16.h),
+          const AddressLineContainer(
             showBorder: false,
             title: "City",
             subtitle: "Orlando",
           ),
-          AddressLineContainer(
+          SizedBox(height: 16.h),
+          const AddressLineContainer(
             showBorder: false,
             title: "Zip Code",
             subtitle: "32837",
           ),
-          AddressLineContainer(
+          SizedBox(height: 16.h),
+          const AddressLineContainer(
             showBorder: false,
             title: "Phone",
             subtitle: "+1(689) 209-8652",
