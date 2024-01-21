68d2c3cef804fe586f4d3191e681c5b0dcd905d7
sparkwud <kelvinagbenyo@gmail.com>
2024-01-21 17:46:57 +0000

    forgot password controller provider (proper)

--- lib/providers/forgot_password_provider.dart
+++ lib/providers/forgot_password_provider.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:golly_express/network/api/response_models/forgot_password_model.dart';
 import 'package:golly_express/network/api/services/api_service.dart';
 
+typedef ForgotPasswordNotifier
+    = AsyncNotifierProvider<ForgotPasswordController, ForgotPasswordModel?>;
+
 final forgotPasswordProvider =
-    FutureProvider.family.autoDispose((ref, String email) async {
-  final forgotPassword =
-      ref.watch(apiServiceProvider).forgotPassword(email: email);
-  // ref.read(resetTokenProvider.notifier).state = await forgotPassword;
-  return forgotPassword;
-});
+    ForgotPasswordNotifier(() => ForgotPasswordController());
 
-final resetPasswordEmail = StateProvider((_) => "");
+// final apiServiceProvider = Provider((ref) => GollyApiService());
+class ForgotPasswordController extends AsyncNotifier<ForgotPasswordModel?> {
+  @override
+  FutureOr<ForgotPasswordModel?> build() {
+    return null;
+  }
 
-final resetTokenProvider = StateProvider((_) => "");
+  Future<ForgotPasswordModel?> forgotPassword({required String email}) async {
+    final apiService = ref.read(apiServiceProvider);
+    state = const AsyncLoading();
 
-final isLoadingProvider = StateProvider<bool>((_) => false);
+    state = await AsyncValue.guard(
+      () => apiService.forgotPassword(email: email),
+    );
+    return state.value;
+  }
+}
