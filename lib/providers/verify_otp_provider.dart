35d94fef29e2a7a30dd25fb581cc1fe730dd3a52
sparkwud <kelvinagbenyo@gmail.com>
2024-01-21 17:47:58 +0000

    verify otp controller provider (proper)

--- lib/providers/verify_otp_provider.dart
+++ lib/providers/verify_otp_provider.dart
@@ -1,14 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:golly_express/network/api/request_models/otp_request.dart';
+import 'package:golly_express/network/api/response_models/otp_response.dart';
 import 'package:golly_express/network/api/services/api_service.dart';
 
-final verifyOtpProvider =
-    FutureProvider.family.autoDispose((ref, OtpRequest requestBody) async {
-  final verifyOtp =
-      ref.watch(apiServiceProvider).verifyOtp(requestBody: requestBody);
-  return verifyOtp;
-});
+final enableButtonProvider = StateProvider((ref) => false);
 
-final enableButtonProvider = StateProvider((_) => false);
+typedef VerifyOtpNotifier
+    = AsyncNotifierProvider<VerifyOtpController, OtpResponse?>;
 
-final otpProvider = StateProvider((_) => "");
+final verifyOtpProvider = VerifyOtpNotifier(() => VerifyOtpController());
+
+class VerifyOtpController extends AsyncNotifier<OtpResponse?> {
+  @override
+  FutureOr<OtpResponse?> build() {
+    return null;
+  }
+
+  Future<OtpResponse?> verifyOtp({required OtpRequest requestBody}) async {
+    final apiService = ref.read(apiServiceProvider);
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(
+      () => apiService.verifyOtp(requestBody: requestBody),
+    );
+    return state.value;
+  }
+}
