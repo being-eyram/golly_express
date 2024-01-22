8a8d0cadd79a7e2bbb3317db60ae002a0cfdf529
sparkwud <kelvinagbenyo@gmail.com>
2024-01-22 06:31:31 +0000

    make component responsive

--- lib/components/tracking_number_field.dart
+++ lib/components/tracking_number_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TrackingNumberSearchField extends StatelessWidget {
   const TrackingNumberSearchField({super.key});
@@ -11,53 +12,54 @@ class TrackingNumberSearchField extends StatelessWidget {
         TextFormField(
           onTapOutside: (event) =>
               FocusManager.instance.primaryFocus?.unfocus(),
-
           enabled: true,
-          // decoration: InputDecoration(enabled: true, ),
           decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 16.w,
+              vertical: 16.h,
+            ),
+            isDense: true,
             filled: true,
             fillColor: Colors.white,
             hintText: 'Tracking Number',
-            hintStyle: const TextStyle(
-              fontSize: 15,
+            hintStyle: TextStyle(
+              fontSize: 14.sp,
               fontWeight: FontWeight.w400,
             ),
-            contentPadding: const EdgeInsets.only(left: 16.0),
             enabledBorder: OutlineInputBorder(
               borderSide: const BorderSide(
                 color: Colors.white,
                 width: 1,
               ),
-              borderRadius: BorderRadius.circular(12.0),
+              borderRadius: BorderRadius.circular(12.r),
             ),
             focusedBorder: OutlineInputBorder(
               borderSide: const BorderSide(
                 color: Colors.white,
                 width: 1,
               ),
-              borderRadius: BorderRadius.circular(12.0),
+              borderRadius: BorderRadius.circular(12.r),
             ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(right: 5.0),
-          child: Container(
-            alignment: Alignment.center,
-            height: 41,
-            width: 41,
-            decoration: BoxDecoration(
-              color: const Color(0xFF557A46),
-              borderRadius: BorderRadius.circular(12),
+        Container(
+          margin: EdgeInsets.only(right: 5.w),
+          alignment: Alignment.center,
+          height: 35.w,
+          width: 35.w,
+          // padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: const Color(0xFF557A46),
+            borderRadius: BorderRadius.circular(12.r),
+          ),
+          child: IconButton(
+            padding: EdgeInsets.zero,
+            icon: Icon(
+              Icons.search,
+              size: 28.w,
             ),
-            child: IconButton(
-              padding: EdgeInsets.zero,
-              icon: const Icon(
-                Icons.search,
-                size: 30,
-              ),
-              color: Colors.white,
-              onPressed: () {},
-            ),
+            color: Colors.white,
+            onPressed: () {},
           ),
         )
       ],
