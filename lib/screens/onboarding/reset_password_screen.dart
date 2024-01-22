b4bce04a95ff86e315f0fe5c791e61e756bbd6a7
sparkwud <kelvinagbenyo@gmail.com>
2024-01-22 09:10:05 +0000

    reset password screen (proper)

--- lib/screens/onboarding/reset_password_screen.dart
+++ lib/screens/onboarding/reset_password_screen.dart
@@ -4,108 +4,161 @@ import 'package:go_router/go_router.dart';
 import 'package:golly_express/components/custom_button.dart';
 import 'package:golly_express/components/input_field.dart';
 import 'package:golly_express/constants.dart';
+import 'package:golly_express/providers/reset_password_provider.dart';
+import 'package:golly_express/shared/app_routes.dart';
+import 'package:golly_express/shared/extensions/context_extension.dart';
 import 'package:golly_express/shared/extensions/field_validation.dart';
+import 'package:golly_express/shared/utils/show_dialog.dart';
 
-class ResetPasswordScreen extends ConsumerWidget {
-  const ResetPasswordScreen({super.key});
+import '../../network/api/request_models/reset_password_request.dart';
+
+class ResetPasswordScreen extends ConsumerStatefulWidget {
+  const ResetPasswordScreen({
+    super.key,
+    required this.resetToken,
+  });
+  final String resetToken;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final formKey = GlobalKey<FormState>();
+  ConsumerState<ResetPasswordScreen> createState() =>
+      _ResetPasswordScreenState();
+}
 
+class _ResetPasswordScreenState extends ConsumerState<ResetPasswordScreen> {
+  static final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
     var confirmPassword = "";
 
+    final resetState = ref.watch(resetPasswordProvider);
+    ref.listen(resetPasswordProvider, (previous, next) {
+      next.when(
+        data: (data) {
+          debugPrint(data?.message);
+          dialogg(
+            context,
+            barrierDismissible: false,
+            icon: Icons.arrow_forward,
+            title: 'Success',
+            content: "Password reset successful. proceed to login.",
+            onPressed: () => context.go(AppRoutes.login),
+          );
+        },
+        error: (error, stackTrace) {
+          final errMessage = error.toString().split(':')[1].trim();
+          const errorMessage = "Failed to reset password. Please try again.";
+          debugPrint("Error: $errMessage");
+
+          dialogg(
+            context,
+            // barrierDismissible: false,
+            title: 'Error',
+            content: errorMessage,
+            onPressed: () => context.go(AppRoutes.login),
+          );
+        },
+        loading: () {},
+      );
+    });
+
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => context.pop(),
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            size: 20,
-          ),
-        ),
-      ),
-      resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                gollyExpressLogo,
-                const Text(
-                  "Enter New Password",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.w800,
+        child: SizedBox(
+          height: context.height,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: context.height * .2),
+                      Transform.scale(
+                        scale: 1.5,
+                        child: gollyExpressLogo,
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        "Enter New Password",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        "Please enter your new password",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      InputTextField(
+                        validator: (value) {
+                          confirmPassword = value;
+                          return validatePassword(value);
+                        },
+                        hintText: "Enter New Password",
+                        labelText: "Password",
+                        isPasswordInput: true,
+                        isObscured: ref.watch(showPasswordProvider),
+                        suffixIconOnTap: () {
+                          ref
+                              .read(showPasswordProvider.notifier)
+                              .update((state) => !state);
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      InputTextField(
+                        validator: (value) {
+                          if (value != confirmPassword) {
+                            return "Passwords do not match";
+                          }
+                          if (value.isEmpty) {
+                            return 'Password cannot be empty';
+                          }
+                          return null;
+                        },
+                        hintText: "Confirm Password",
+                        labelText: "Confirm Password",
+                        isPasswordInput: true,
+                        isObscured: ref.watch(showPasswordProvider),
+                        suffixIconOnTap: () {
+                          ref
+                              .read(showPasswordProvider.notifier)
+                              .update((state) => !state);
+                        },
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  "Please enter your new password",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 24),
+                    child: CustomButton(
+                      borderRadius: 8,
+                      isEnabled: true,
+                      isLoading: resetState.isLoading,
+                      buttonText: "Reset Password",
+                      onPressed: () async {
+                        if (!formKey.currentState!.validate()) return;
+
+                        final resetBody = ResetPasswordRequest(
+                          resetToken: widget.resetToken,
+                          password: confirmPassword,
+                        );
+                        ref
+                            .read(resetPasswordProvider.notifier)
+                            .resetPassword(requestBody: resetBody);
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(height: 40),
-                InputTextField(
-                  validator: (value) {
-                    confirmPassword = value;
-                    return validatePassword(value);
-                  },
-                  hintText: "Enter Password",
-                  isPasswordInput: true,
-                  isObscured: ref.watch(showPasswordProvider),
-                  suffixIconOnTap: () {
-                    ref
-                        .read(showPasswordProvider.notifier)
-                        .update((state) => !state);
-                  },
-                ),
-                const SizedBox(height: 24),
-                InputTextField(
-                  validator: (value) {
-                    if (value != confirmPassword) {
-                      return "Passwords do not match";
-                    }
-                    if (value.isEmpty) {
-                      return 'Password cannot be empty';
-                    }
-                    return null;
-                  },
-                  hintText: "Confirm Password",
-                  isPasswordInput: true,
-                  isObscured: ref.watch(showPasswordProvider),
-                  suffixIconOnTap: () {
-                    ref
-                        .read(showPasswordProvider.notifier)
-                        .update((state) => !state);
-                  },
-                ),
-                const SizedBox(height: 32),
-                const Divider(
-                  color: Color(0xFFEDEFEE),
-                  // height: 16,
-                ),
-                CustomButton(
-                  borderRadius: 8,
-                  isEnabled: true,
-                  buttonText: "Reset Password",
-                  onPressed: () async {
-                    if (!formKey.currentState!.validate()) return;
-                    // ref
-                    //     .read(asyncAuthProvider.notifier)
-                    //     .signupUser(userCredentials);
-                  },
-                ),
-                const SizedBox(height: 50)
-              ],
+                ],
+              ),
             ),
           ),
         ),
