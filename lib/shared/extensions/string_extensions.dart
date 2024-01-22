3c30b3ef7c36fb6ac26515fed007d3a7f9cb066f
sparkwud <kelvinagbenyo@gmail.com>
2024-01-22 06:43:21 +0000

    greeting string extension (still needs clean up)

--- lib/shared/extensions/string_extensions.dart
+++ lib/shared/extensions/string_extensions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 extension EmailValidator on String {
   bool isValidEmail() {
@@ -19,10 +20,11 @@ extension StringCasingExtension on String {
   String? trimToken() => contains(":") ? split(":")[1].trim() : this;
   String? trimSpaces() => replaceAll(" ", "");
   String removeSpacesAndLower() => replaceAll(' ', '').toLowerCase();
+  String get first => split(" ")[0];
 }
 
 extension Greetings on String {
-  String greet() {
+  String get greetFirstName {
     final hour = TimeOfDay.now().hour;
     String timeOfDay = "";
     if (hour <= 12) {
@@ -35,6 +37,24 @@ extension Greetings on String {
     return "Good $timeOfDay, ${split(" ")[0]}";
   }
 }
+
+final currentTimeProvider = Provider<DateTime>((ref) => DateTime.now());
+
+final greetingProvider = Provider((ref) {
+  // final hour = TimeOfDay.now().hour;
+  final currentTime = ref.watch(currentTimeProvider);
+  final hour = currentTime.hour;
+  String timeOfDay = "";
+  if (hour < 12) {
+    timeOfDay = "Morning";
+  } else if (hour < 17) {
+    timeOfDay = "Afternoon";
+  } else {
+    timeOfDay = "Evening";
+  }
+  return "Good $timeOfDay";
+});
+
 
   // String greeting(name) {
   //   final hour = TimeOfDay.now().hour;
