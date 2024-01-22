4e36ea8a11c8f744294cf516e6a63ea62ca959fe
sparkwud <kelvinagbenyo@gmail.com>
2024-01-22 06:38:43 +0000

    fix keyboard auto hide issue caused by screen util

--- lib/screens/onboarding/register_screen.dart
+++ lib/screens/onboarding/register_screen.dart
@@ -1,135 +1,160 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:golly_express/components/custom_button.dart';
 import 'package:golly_express/components/input_field.dart';
-import 'package:golly_express/constants.dart';
-import 'package:golly_express/extensions/field_validation.dart';
-import 'package:golly_express/providers/user_data_provider.dart';
+import 'package:golly_express/network/api/request_models/auth_request_models.dart';
+import 'package:golly_express/shared/app_routes.dart';
+import 'package:golly_express/shared/custom_input_formatters.dart';
+import 'package:golly_express/shared/extensions/context_extension.dart';
+import 'package:golly_express/shared/extensions/field_validation.dart';
 
-class RegisterScreen extends ConsumerWidget {
+class RegisterScreen extends ConsumerStatefulWidget {
   const RegisterScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final formKey = GlobalKey<FormState>();
+  ConsumerState<RegisterScreen> createState() => _RegisterScreenState();
+}
 
-    final emailController = TextEditingController();
-    final fullNameController = TextEditingController();
-    final phoneNumberController = TextEditingController();
+class _RegisterScreenState extends ConsumerState<RegisterScreen> {
+  late TextEditingController emailController;
+  late TextEditingController fullNameController;
+  late TextEditingController phoneNumberController;
+  final formKey = GlobalKey<FormState>();
 
-    // var email = ref.watch(emailProvider);
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    fullNameController = TextEditingController();
+    phoneNumberController = TextEditingController();
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           onPressed: () => context.pop(),
-          icon: const Icon(
+          icon: Icon(
             Icons.arrow_back_ios_new,
-            size: 20,
+            size: 20.h,
           ),
         ),
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Container(
+          height: context.height,
+          padding: EdgeInsets.symmetric(horizontal: 24.w).copyWith(bottom: 100),
           child: Form(
             key: formKey,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                gollyExpressLogo,
-                const Text(
-                  "Register to start",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  "Enter your information below",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                const SizedBox(height: 30),
-
-                // email address
-                InputTextField(
-                  // autovalidateMode: AutovalidateMode.onUserInteraction,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: emailController,
-                  labelText: "Email",
-                  hintText: "Enter Email",
-
-                  validator: (value) => validateEmail(value),
-                ),
-                const SizedBox(height: 20),
-
-                // full name
-                InputTextField(
-                  // autovalidateMode: AutovalidateMode.onUserInteraction,
-                  labelText: "Full Name",
-                  hintText: "Enter your full name",
-                  controller: fullNameController,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r"[a-zA-Z]+|\s+|\-"),
-                    )
-                  ],
-                  validator: (value) => validateFullName(value),
-                ),
-                const SizedBox(height: 20),
-
-                // phone number
-                InputTextField(
-                  controller: phoneNumberController,
-                  validator: (value) => validatePhoneNumber(value),
-                  hintText: "Phone Number",
-                  prefixIcon: const Padding(
-                    padding: EdgeInsets.only(
-                        top: 15, bottom: 15, left: 15, right: 10),
-                    child: Text(
-                      "+233 ",
+                Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/golly_express.jpeg',
+                      height: 91.h,
+                    ),
+                    Text(
+                      "Register to start",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 24.sp,
                         fontWeight: FontWeight.w800,
                       ),
                     ),
-                  ),
-                  keyboardType: TextInputType.phone,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r"[0-9]")),
-                    LengthLimitingTextInputFormatter(9)
+                    SizedBox(height: 8.h),
+                    Text(
+                      "Enter your information below",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
                   ],
                 ),
-                const SizedBox(height: 30),
-                const Divider(
-                  color: Color(0xFFEDEFEE),
-                  height: 0,
+
+                Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // EMAIL ADDRESS FIELD
+                    InputTextField(
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      labelText: "Email Address",
+                      hintText: "Enter Email",
+                      validator: (value) => validateEmail(value),
+                    ),
+                    SizedBox(height: 30.h),
+
+                    // FULL NAME FIELD
+                    InputTextField(
+                      labelText: "Full Name",
+                      hintText: "Enter your full name",
+                      controller: fullNameController,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                          RegExp(r"[a-zA-Z]+|\s+|\-"),
+                        ),
+                        NameFormatter(),
+                      ],
+                      validator: (value) => validateFullName(value),
+                    ),
+                    SizedBox(height: 30.h),
+
+                    // PHONE NUMBER FIELD
+                    InputTextField(
+                      controller: phoneNumberController,
+                      validator: (value) => validatePhoneNumber(value),
+                      hintText: "Phone Number",
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(
+                            top: 15.h, bottom: 15.h, left: 15.w, right: 10.w),
+                        child: Text(
+                          "+233 ",
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.phone,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp(r"[0-9]")),
+                        LengthLimitingTextInputFormatter(9),
+                        PhoneNumberInputFormatter(),
+                      ],
+                    ),
+                  ],
                 ),
+
+                // NEXT BUTTON
                 CustomButton(
-                  borderRadius: 8,
+                  borderRadius: 8.r,
                   isEnabled: true,
                   buttonText: "Next",
                   onPressed: () {
                     if (formKey.currentState!.validate()) {
-                      ref.read(emailProvider.notifier).state =
-                          emailController.text;
-                      ref.read(phoneNumberProvider.notifier).state =
-                          phoneNumberController.text;
-                      ref.read(fullNameProvider.notifier).state =
-                          fullNameController.text;
-                      context.push("/registerPassword");
+                      AuthRequest req = AuthRequest(
+                        email: emailController.text,
+                        phoneNumber: phoneNumberController.text,
+                        fullName: fullNameController.text,
+                        password: "",
+                      );
+                      context.push(
+                        AppRoutes.registerPassword,
+                        extra: req,
+                      );
                     }
                   },
                 ),
-                const SizedBox(height: 50)
               ],
             ),
           ),
