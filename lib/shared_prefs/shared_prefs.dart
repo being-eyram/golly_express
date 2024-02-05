07104181a4efdfe64d151d6199194558f27cc9c9
sparkwud <kelvinagbenyo@gmail.com>
2024-02-05 23:15:23 +0000

    add login status getters and setters to shared prefs

--- lib/shared_prefs/shared_prefs.dart
+++ lib/shared_prefs/shared_prefs.dart
@@ -10,6 +10,22 @@ Future<bool?> getUserOnboardStatus() async {
   return prefs.getBool('is_onboarded');
 }
 
+setLoginStatus(bool value) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setBool("isLoggedIn", value);
+}
+
+Future<bool?> getLoginStatus() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getBool("isLoggedIn");
+}
+
+Future<bool> loginStatus() async {
+  final loginStatus = await getUserBearerToken();
+
+  return loginStatus != null;
+}
+
 setUserBearerToken(String value) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.setString('bearer_token', value);
