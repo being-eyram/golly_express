204edf12715ce15a292af1535d30efcc105d689a
sparkwud <kelvinagbenyo@gmail.com>
2024-02-08 21:06:08 +0000

    fix user registration flow

--- lib/screens/onboarding/register_password_screen.dart
+++ lib/screens/onboarding/register_password_screen.dart
@@ -4,51 +4,53 @@ import 'package:go_router/go_router.dart';
 import 'package:golly_express/components/custom_button.dart';
 import 'package:golly_express/components/input_field.dart';
 import 'package:golly_express/constants.dart';
-import 'package:golly_express/extensions/field_validation.dart';
 import 'package:golly_express/network/api/request_models/auth_request_models.dart';
-import 'package:golly_express/providers/user_data_provider.dart';
+import 'package:golly_express/shared/app_routes.dart';
+import 'package:golly_express/shared/extensions/context_extension.dart';
+import 'package:golly_express/shared/extensions/field_validation.dart';
+import 'package:golly_express/shared/utils/show_dialog.dart';
 
 import '../../providers/auth_provider.dart';
 
-class RegisterPasswordScreen extends ConsumerWidget {
-  const RegisterPasswordScreen({super.key});
+class RegisterPasswordScreen extends ConsumerStatefulWidget {
+  const RegisterPasswordScreen({
+    super.key,
+    required this.userCred,
+  });
+
+  final AuthRequest userCred;
+  @override
+  ConsumerState<RegisterPasswordScreen> createState() =>
+      _RegisterPasswordScreenState();
+}
+
+class _RegisterPasswordScreenState
+    extends ConsumerState<RegisterPasswordScreen> {
+  static final formKey = GlobalKey<FormState>();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final formKey = GlobalKey<FormState>();
-
-    final email = ref.watch(emailProvider);
-    final fullName = ref.watch(fullNameProvider);
-    final phoneNumber = ref.watch(phoneNumberProvider);
+  Widget build(BuildContext context) {
     var confirmPassword = "";
 
-    final isLoading = ref.watch(asyncAuthProvider).isLoading;
+    final showPassword = ref.watch(showPasswordProvider);
+    final signupState = ref.watch(authControllerProvider); //isLoading
 
-    final userCredentials = AuthRequest(
-      email: email,
-      password: confirmPassword,
-      fullName: fullName,
-      phoneNumber: phoneNumber,
-    );
-
-    ref.listen(asyncAuthProvider, (_, signupState) {
+    ref.listen(authControllerProvider, (previous, signupState) {
       signupState.when(
-        data: (_) => context.go('/login'),
-        error: (errMessage, __) {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) => AlertDialog(
-              title: const Text('Error'),
-              content: Text(errMessage.toString()),
-              actions: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            ),
+        data: (_) {
+          debugPrint("Account created successfully");
+          successDialogg(
+            context,
+            content: "Account created successfully. Proceed to login page...",
+            onPressed: () => context.go(AppRoutes.login),
+          );
+        },
+        error: (error, __) {
+          debugPrint("Failed to create new account");
+          final errMessage = error.toString().split(':')[1].trim();
+          errorDialogg(
+            context,
+            content: errMessage,
           );
         },
         loading: () {},
@@ -67,85 +69,99 @@ class RegisterPasswordScreen extends ConsumerWidget {
       ),
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: context.height,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24),
             child: Form(
               key: formKey,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  gollyExpressLogo,
-                  const Text(
-                    "Enter Password",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800,
+                  Expanded(
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.scale(
+                          scale: 1.5,
+                          child: gollyExpressLogo,
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          "Enter Password",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Choose a password to secure account",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        InputTextField(
+                          validator: (value) {
+                            confirmPassword = value;
+                            return validatePassword(value);
+                          },
+                          hintText: "Enter Password",
+                          isPasswordInput: true,
+                          isObscured: showPassword,
+                          suffixIconOnTap: () {
+                            ref
+                                .read(showPasswordProvider.notifier)
+                                .update((state) => !state);
+                          },
+                        ),
+                        const SizedBox(height: 24),
+                        InputTextField(
+                          validator: (value) =>
+                              validateConfirmPassword(value, confirmPassword),
+                          hintText: "Confirm Password",
+                          isPasswordInput: true,
+                          isObscured: showPassword,
+                          suffixIconOnTap: () {
+                            ref
+                                .read(showPasswordProvider.notifier)
+                                .update((state) => !state);
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        // const Divider(
+                        //   color: Color(0xFFEDEFEE),
+                        //   // height: 16,
+                        // ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Choose a password to secure account",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 24),
+                    child: CustomButton(
+                      borderRadius: 8,
+                      isEnabled: true,
+                      isLoading: signupState.isLoading,
+                      buttonText: "Next",
+                      onPressed: () async {
+                        if (!formKey.currentState!.validate()) return;
+                        final userCredentials = AuthRequest(
+                          email: widget.userCred.email,
+                          password: confirmPassword,
+                          fullName: widget.userCred.fullName,
+                          phoneNumber: widget.userCred.phoneNumber,
+                        );
+                        ref
+                            .read(authControllerProvider.notifier)
+                            .signupUser(userCredentials);
+                      },
                     ),
                   ),
-                  const SizedBox(height: 40),
-                  InputTextField(
-                    validator: (value) {
-                      confirmPassword = value;
-                      return validatePassword(value);
-                    },
-                    hintText: "Enter Password",
-                    isPasswordInput: true,
-                    isObscured: ref.watch(showPasswordProvider),
-                    suffixIconOnTap: () {
-                      ref
-                          .read(showPasswordProvider.notifier)
-                          .update((state) => !state);
-                    },
-                  ),
-                  const SizedBox(height: 24),
-                  InputTextField(
-                    validator: (value) {
-                      if (value != confirmPassword) {
-                        return "Passwords do not match";
-                      }
-                      if (value.isEmpty) {
-                        return 'Password cannot be empty';
-                      }
-                      return null;
-                    },
-                    hintText: "Confirm Password",
-                    isPasswordInput: true,
-                    isObscured: ref.watch(showPasswordProvider),
-                    suffixIconOnTap: () {
-                      ref
-                          .read(showPasswordProvider.notifier)
-                          .update((state) => !state);
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  const Divider(
-                    color: Color(0xFFEDEFEE),
-                    // height: 16,
-                  ),
-                  CustomButton(
-                    borderRadius: 8,
-                    isEnabled: true,
-                    buttonText: "Next",
-                    onPressed: () async {
-                      if (!formKey.currentState!.validate()) return;
-                      ref
-                          .read(asyncAuthProvider.notifier)
-                          .signupUser(userCredentials);
-                    },
-                  ),
-                  const SizedBox(height: 50)
                 ],
               ),
             ),
